828136a2de952b3e2fa7e23924246a317872cf54
giraluna <giraluna@users.noreply.github.com>
2014-12-05 16:22:36 +0200

    reorganize emblems & add a bunch of new ones

--- data/templates/subemblemtemplates.ts
+++ data/templates/subemblemtemplates.ts
@@ -11,73 +11,186 @@ module Rance
 
     export module SubEmblems
     {
-      export var comm =
+      export var emblem0 =
       {
         type: "both",
         foregroundOnly: true,
-        imageSrc: "comm.png"
+        imageSrc: "emblem0.png"
       }
-      export var comm3 =
+      export var emblem32 =
       {
         type: "both",
         foregroundOnly: true,
-        imageSrc: "comm3.png"
+        imageSrc: "emblem32.png"
       }
-      export var fasc12 =
+      export var emblem33 =
       {
         type: "both",
         foregroundOnly: true,
-        imageSrc: "fasc12.png"
+        imageSrc: "emblem33.png"
       }
-      export var fasc2 =
+      export var emblem34 =
       {
         type: "both",
         foregroundOnly: true,
-        imageSrc: "fasc2.png"
+        imageSrc: "emblem34.png"
       }
-      export var fasc8 =
+      export var emblem35 =
       {
         type: "both",
         foregroundOnly: true,
-        imageSrc: "fasc8.png"
+        imageSrc: "emblem35.png"
       }
-      export var fasc9 =
+      export var emblem36 =
       {
         type: "both",
         foregroundOnly: true,
-        imageSrc: "fasc9.png"
+        imageSrc: "emblem36.png"
       }
-      export var mon13 =
+      export var emblem37 =
       {
         type: "both",
         foregroundOnly: true,
-        imageSrc: "mon13.png"
+        imageSrc: "emblem37.png"
       }
-      export var mon16 =
+      export var emblem38 =
       {
         type: "both",
         foregroundOnly: true,
-        imageSrc: "mon16.png"
+        imageSrc: "emblem38.png"
       }
-      export var mon18 =
+      export var emblem39 =
       {
         type: "both",
         foregroundOnly: true,
-        imageSrc: "mon18.png"
+        imageSrc: "emblem39.png"
       }
-      export var mon26 =
+      export var emblem40 =
       {
         type: "both",
         foregroundOnly: true,
-        imageSrc: "mon26.png"
+        imageSrc: "emblem40.png"
       }
-      export var mon9 =
+      export var emblem41 =
       {
         type: "both",
         foregroundOnly: true,
-        imageSrc: "mon9.png"
+        imageSrc: "emblem41.png"
       }
-
+      export var emblem42 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem42.png"
+      }
+      export var emblem43 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem43.png"
+      }
+      export var emblem44 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem44.png"
+      }
+      export var emblem45 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem45.png"
+      }
+      export var emblem46 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem46.png"
+      }
+      export var emblem47 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem47.png"
+      }
+      export var emblem48 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem48.png"
+      }
+      export var emblem49 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem49.png"
+      }
+      export var emblem50 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem50.png"
+      }
+      export var emblem51 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem51.png"
+      }
+      export var emblem52 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem52.png"
+      }
+      export var emblem53 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem53.png"
+      }
+      export var emblem54 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem54.png"
+      }
+      export var emblem55 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem55.png"
+      }
+      export var emblem56 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem56.png"
+      }
+      export var emblem57 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem57.png"
+      }
+      export var emblem58 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem58.png"
+      }
+      export var emblem59 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem59.png"
+      }
+      export var emblem61 =
+      {
+        type: "both",
+        foregroundOnly: true,
+        imageSrc: "emblem61.png"
+      }
     }
   }
 }
---
+++ emblemscript.py
@@ -0,0 +1,27 @@
+import os
+
+os.chdir("img/emblems")
+
+i = 0
+result = open("emblems", "a")
+
+def makeEmblemTemplate(name):
+  emblemTemplate = (
+    'export var ' + name +' =\n'
+    '{\n'
+    '  type: "both",\n'
+    '  foregroundOnly: true,\n'
+    '  imageSrc: "' + name + '.png"\n'
+    '}\n'
+  )
+
+  return emblemTemplate;
+
+
+for filename in os.listdir("."):
+  if filename.endswith(".png"):
+    # newName = "emblem" + str(i)
+    # os.rename(filename, newName + ".png")
+    result.write(makeEmblemTemplate(filename[:-4]))
+
+result.close()
