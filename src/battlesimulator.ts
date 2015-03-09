54a19187c740e743be1f0ebe98ce6ba2b2688c87
giraluna <giraluna@users.noreply.github.com>
2015-03-09 16:05:37 +0200

    Class for simulating battle results

---
+++ src/battlesimulator.ts
@@ -0,0 +1,59 @@
+/// <reference path="battle.ts"/>
+/// <reference path="mctree.ts"/>
+
+module Rance
+{
+  export class BattleSimulator
+  {
+    battle: Battle;
+    moveSimulationDepth: number;
+
+    constructor(battle: Battle, moveSimulationDepth: number)
+    {
+      this.battle = battle;
+      this.moveSimulationDepth = moveSimulationDepth;
+    }
+
+    simulateBattle()
+    {
+      while (!this.battle.ended)
+      {
+        this.simulateMove();
+      }
+    }
+
+    simulateMove()
+    {
+      if (!this.battle.activeUnit || this.battle.ended)
+      {
+        throw new Error("Simulated battle already ended");
+      }
+
+      var tree = new MCTree(this.battle, this.battle.activeUnit.battleStats.side);
+
+      var move = tree.evaluate(this.moveSimulationDepth).move;
+      var target = this.battle.unitsById[move.targetId];
+
+      this.simulateAbility(move.ability, target);
+      
+      this.battle.endTurn();
+    }
+
+    simulateAbility(ability: Templates.IAbilityTemplate, target: Unit)
+    {
+      useAbility(this.battle, this.battle.activeUnit, ability, target);
+    }
+
+    getBattleEndData()
+    {
+      if (!this.battle.ended)
+      {
+        throw new Error("Simulated battle hasn't ended yet");
+      }
+
+      var captured = this.battle.capturedUnits;
+      var destroyed = this.battle.deadUnits;
+      var victor = this.battle.getVictor();
+    }
+  }
+}
