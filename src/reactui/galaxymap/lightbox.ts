05876a585e1a09c07a448481152b8f8bc9863519
giraluna <giraluna@users.noreply.github.com>
2015-03-22 15:15:31 +0200

    Automatically add horizontal padding to light box if theres space

--- src/reactui/galaxymap/lightbox.ts
+++ src/reactui/galaxymap/lightbox.ts
@@ -6,6 +6,38 @@ module Rance
     {
       displayName: "LightBox",
 
+      // far from ideal as it always triggers reflow twice
+      // cant figure out how to do resizing better since content size is dynamic
+      handleResize: function()
+      {
+        var container = this.refs.container.getDOMNode();
+        container.classList.remove("light-box-horizontal-padding");
+        container.classList.remove("light-box-fill-horizontal");
+
+        if (container.getBoundingClientRect().width + 10 < window.innerWidth)
+        {
+          container.classList.add("light-box-horizontal-padding");
+        }
+        else
+        {
+          container.classList.add("light-box-fill-horizontal");
+        }
+      },
+
+      componentDidMount: function()
+      {
+        window.addEventListener("resize", this.handleResize, false);
+        this.handleResize();
+      },
+      componentWillUnmount: function()
+      {
+        window.removeEventListener("resize", this.handleResize);
+      },
+      componentDidUpdate: function()
+      {
+        this.handleResize();
+      },
+
       render: function()
       {
         return(
@@ -15,7 +47,8 @@ module Rance
           },
             React.DOM.div(
             {
-              className: "light-box-container"
+              className: "light-box-container",
+              ref: "container"
             },
               React.DOM.button(
               {
