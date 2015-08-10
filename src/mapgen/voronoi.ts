b12335ca1e776ddf402c621f26f7b480a48fc266
giraluna <giraluna3@gmail.com>
2015-08-10 13:51:08 +0300

    begin separating voronoi functions

---
+++ src/mapgen/voronoi.ts
@@ -0,0 +1,76 @@
+/// <reference path="../../lib/voronoi.d.ts" />
+
+/// <reference path="../point.ts" />
+
+module Rance
+{
+  export module MapGen
+  {
+    export function makeVoronoi(points: Point[], width: number, height: number)
+    {
+      var boundingBox =
+      {
+        xl: 0,
+        xr: width,
+        yt: 0,
+        yb: height
+      };
+
+      var voronoi = new Voronoi();
+      var diagram = voronoi.compute(points, boundingBox);
+
+      for (var i = 0; i < diagram.cells.length; i++)
+      {
+        var cell = diagram.cells[i];
+        cell.site.voronoiCell = cell;
+        cell.vertices = getVerticesFromCell(cell);
+      }
+
+      return diagram;
+    }
+
+    /**
+     * Perform one iteration of Lloyd's Algorithm to move points in voronoi diagram to their centroid
+     * @param {any}             diagram Voronoi diagram to relax
+     * @param {(any) => number} dampeningFunction If specified, use value returned by dampeningFunction(cell.site)
+     *                                            to adjust how far towards centroid the point is moved.
+     *                                            0.0 = not moved, 0.5 = moved halfway, 1.0 = moved fully
+     */
+    export function relaxVoronoi(diagram: any, dampeningFunction?: (any) => number)
+    {
+      var relaxedPoints = [];
+
+      for (var i = 0; i < diagram.cells.length; i++)
+      {
+        var cell = diagram.cells[i];
+        var point = cell.site;
+        var centroid = getCentroid(cell.vertices);
+        if (dampeningFunction)
+        {
+          var dampeningValue = dampeningFunction(point);
+
+          var xDelta = (centroid.x - point.x) * dampeningValue;
+          var yDelta = (centroid.y - point.y) * dampeningValue;
+
+          point.setPosition(point.x + xDelta, point.y + yDelta);
+        }
+        else
+        {
+          point.setPosition(centroid.x, centroid.y);
+        }
+      }
+    }
+
+    function getVerticesFromCell(cell: any)
+    {
+      var vertices = [];
+
+      for (var i = 0; i < cell.halfedges.length; i++)
+      {
+        vertices.push(cell.halfedges[i].getStartpoint());
+      }
+
+      return vertices;
+    }
+  }
+}
