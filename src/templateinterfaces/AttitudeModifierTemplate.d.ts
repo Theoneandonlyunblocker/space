d2336b9745a1694603ea8234d336b6f68e1f043b
giraluna <giraluna3@gmail.com>
2016-04-01 23:22:53 +0300

    Move AttitudeModifierTemplate

---
+++ src/templateinterfaces/AttitudeModifierTemplate.d.ts
@@ -0,0 +1,28 @@
+import DiplomacyEvaluation from "../DiplomacyEvaluation.d.ts";
+
+declare interface AttitudeModifierTemplate
+{
+  type: string;
+  displayName: string;
+  // family: AttitudeModifierFamily;
+  duration: number; // -1 === infinite;
+  
+  // if these modifiers are present and one of them has either
+  // stronger effect or opposite sign (+-), don't count this modifier
+  canBeOverriddenBy?: AttitudeModifierTemplate[];
+  
+  triggers?: string[];
+  startCondition?: (evaluation: DiplomacyEvaluation) => boolean;
+  // if endCondition is not defined and duration is infinite, the opposite of startCondition is used
+  // to determine when to end modifier
+  endCondition?: (evaluation: DiplomacyEvaluation) => boolean;
+  
+  constantEffect?: number;
+  getEffectFromEvaluation?: (evaluation: DiplomacyEvaluation) => number;
+  
+  
+  
+  canOverride?: AttitudeModifierTemplate[]; // set dynamically
+}
+
+export default AttitudeModifierTemplate;
