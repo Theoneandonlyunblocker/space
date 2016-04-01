4244afb4e927c5b24e17f58f9666cc359ce0d4cf
giraluna <giraluna3@gmail.com>
2016-04-02 00:18:49 +0300

    Move AbilityEffectTemplate

---
+++ src/templateinterfaces/AbilityEffectTemplate.d.ts
@@ -0,0 +1,17 @@
+import EffectActionTemplate from "./EffectActionTemplate.ts";
+import BattleSFXTemplate from "./BattleSFXTemplate.ts";
+import Unit from "../Unit.ts";
+
+declare interface AbilityEffectTemplate
+{
+  action: EffectActionTemplate;
+  // TODO | pass battle parameter?
+  trigger?: (user: Unit, target: Unit) => boolean;
+  data?: any;
+  // called after parent effect with same user and effect target
+  // nesting these wont work and wouldnt do anything anyway
+  attachedEffects?: AbilityEffectTemplate[];
+  sfx?: BattleSFXTemplate;
+}
+
+export default AbilityEffectTemplate;
