bed56252bcd609d1d56d1523f31aad5f360f64ee
giraluna <giraluna3@gmail.com>
2016-04-02 00:32:58 +0300

    move AbilityBase

--- src/templateinterfaces/AbilityBase.d.ts
+++ src/templateinterfaces/AbilityBase.d.ts
@@ -1,12 +1,14 @@
+import AbilityEffectTemplate from "./AbilityEffectTemplate.d.ts";
+
 // base interface for abilities and passive skills
-declare interface IAbilityBase
+declare interface AbilityBase
 {
   type: string;
   displayName: string;
   description: string;
   isHidden?: boolean;
   // used to check if it's a passive ability or not
-  mainEffect?: IAbilityEffectTemplate;
+  mainEffect?: AbilityEffectTemplate;
 
   // list of ability types this ability can be upgraded into
   // string[] because ability templates referencing ability templates can be a bit messy
@@ -14,3 +16,5 @@ declare interface IAbilityBase
   // if true, can only be upgraded when unit has this ability in it's specialAbilityUpgrades
   onlyAllowExplicitUpgrade?: boolean;
 }
+
+export default AbilityBase;
