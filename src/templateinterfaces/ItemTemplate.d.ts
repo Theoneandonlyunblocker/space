8bae863c17fd311f846b9a74b8183144a8e47e92
giraluna <giraluna3@gmail.com>
2016-04-02 09:40:40 +0300

    move ItemTemplate

---
+++ src/templateinterfaces/ItemTemplate.d.ts
@@ -0,0 +1,34 @@
+/// <reference path="imanufacturablething.d.ts" />
+import ManufacturableThing from "./ManufacturableThing.d.ts";
+import TechnologyRequirement from "./TechnologyRequirement.d.ts";
+import AbilityTemplate from "./AbilityTemplate.d.ts";
+import PassiveSkillTemplate from "./PassiveSkillTemplate.d.ts";
+
+declare interface ItemTemplate extends ManufacturableThing
+{
+  type: string;
+  displayName: string;
+  description: string;
+  icon: string;
+  
+  techLevel: number;
+  
+  slot: string; // low, mid, high
+  
+  buildCost: number;
+  
+  ability?: AbilityTemplate;
+  passiveSkill?: PassiveSkillTemplate;
+  attributes?:
+  {
+    maxActionPoints?: number;
+    attack?: number;
+    defence?: number;
+    intelligence?: number;
+    speed?: number;
+  };
+
+  technologyRequirements?: TechnologyRequirement[];
+}
+
+export default ItemTemplate;
