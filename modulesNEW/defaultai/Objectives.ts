6dd7aeb74864de3cc12b0c12fd20c59fbe3cfff7
giraluna <giraluna3@gmail.com>
2016-04-05 16:02:09 +0300

    add default ai module

---
+++ modulesNEW/defaultai/Objectives.ts
@@ -0,0 +1,10 @@
+import ObjectiveTemplate from "../../src/templateinterfaces/ObjectiveTemplate.d.ts";
+import TemplateCollection from "../../src/templateinterfaces/TemplateCollection.d.ts";
+
+
+const Objectives: TemplateCollection<ObjectiveTemplate> =
+{
+  
+}
+
+export default Objectives;
---
+++ modulesNEW/defaultai/defaultAI.ts
@@ -0,0 +1,26 @@
+import RuleSet from "../../src/RuleSet.ts";
+import ModuleFile from "../../src/ModuleFile.d.ts";
+import ModuleData from "../../src/ModuleData.ts";
+import cacheSpriteSheetAsImages from "../../src/cacheSpriteSheetAsImages.ts";
+import Objectives from "./Objectives.ts";
+import ObjectiveTemplate from "../../src/templateinterfaces/ObjectiveTemplate.d.ts";
+
+const defaultAI: ModuleFile =
+{
+  key: "defaultAI",
+  metaData:
+  {
+    name: "Default AI",
+    version: "0.1.0",
+    author: "giraluna",
+    description: ""
+  },
+  constructModule: function(moduleData: ModuleData)
+  {
+    moduleData.copyTemplates<ObjectiveTemplate>(Objectives, "Objectives");
+    
+    return moduleData;
+  }
+}
+
+export default defaultAI;
