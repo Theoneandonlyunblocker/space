456722970ab081b3552906956f692971a7210cb3
giraluna <giraluna3@gmail.com>
2016-04-09 10:44:07 +0300

    add technologyRequirements prop to ManufacturableThing interface

--- src/templateinterfaces/ManufacturableThing.d.ts
+++ src/templateinterfaces/ManufacturableThing.d.ts
@@ -1,9 +1,14 @@
+import TechnologyRequirement from "./TechnologyRequirement.d.ts";
+
 declare interface ManufacturableThing
 {
   type: string;
   displayName: string;
   description: string;
   buildCost: number;
+  
+  // set dynamically
+  technologyRequirements?: TechnologyRequirement[];
 }
 
 export default ManufacturableThing;
