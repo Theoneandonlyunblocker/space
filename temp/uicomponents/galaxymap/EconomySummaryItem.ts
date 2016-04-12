a5465718f3e078870186c845b62489ed82c47fcd
giraluna <giraluna3@gmail.com>
2016-04-12 18:26:21 +0300

    fix economysummaryitem

--- temp/uicomponents/galaxymap/EconomySummaryItem.ts
+++ temp/uicomponents/galaxymap/EconomySummaryItem.ts
@@ -1,11 +1,19 @@
 /// <reference path="../../../lib/react-0.13.3.d.ts" />
 import * as React from "react";
 
+import ListColumn from "../unitlist/Listcolumn.d.ts";
+import Star from "../../../src/Star.ts";
+
 interface PropTypes extends React.Props<any>
 {
   handleClick: any; // TODO refactor | define prop type 123
   isSelected: boolean;
   activeColumns: ListColumn[];
+  
+  star: Star;
+  id: number;
+  name: string;
+  income: number;
 }
 
 interface StateType
@@ -15,12 +23,26 @@ interface StateType
 export class EconomySummaryItemComponent extends React.Component<PropTypes, StateType>
 {
   displayName: string = "EconomySummaryItem";
+  state: StateType;
 
+  constructor(props: PropTypes)
+  {
+    super(props);
+    
+    this.bindMethods();
+  }
+  private bindMethods()
+  {
+    this.makeCell = this.makeCell.bind(this);    
+  }
+  
   makeCell(type: string)
   {
-    var cellProps: any = {};
-    cellProps.key = type;
-    cellProps.className = "economy-summary-item-cell" + " economy-summary-" + type;
+    var cellProps =
+    {
+      key: type,
+      className: "economy-summary-item-cell" + " economy-summary-" + type,
+    };
 
     var cellContent: any;
 
@@ -38,19 +60,6 @@ export class EconomySummaryItemComponent extends React.Component<PropTypes, Stat
       React.DOM.td(cellProps, cellContent)
     );
   }
-
-  state: StateType;
-
-  constructor(props: PropTypes)
-  {
-    super(props);
-    
-    this.bindMethods();
-  }
-  private bindMethods()
-  {
-    this.makeCell = this.makeCell.bind(this);    
-  }
   
   render()
   {
@@ -65,7 +74,7 @@ export class EconomySummaryItemComponent extends React.Component<PropTypes, Stat
       cells.push(cell);
     }
 
-    var rowProps: any =
+    var rowProps =
     {
       className: "economy-summary-item",
       onClick : this.props.handleClick
