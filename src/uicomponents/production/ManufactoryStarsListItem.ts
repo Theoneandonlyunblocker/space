53d14ff3f347a15613c3c08fe2e920647ed3d363
giraluna <giraluna3@gmail.com>
2016-04-13 17:28:39 +0300

    manufactorystarslistitem

--- src/uicomponents/production/ManufactoryStarsListItem.ts
+++ src/uicomponents/production/ManufactoryStarsListItem.ts
@@ -1,9 +1,6 @@
 /// <reference path="../../../lib/react-0.13.3.d.ts" />
 import * as React from "react";
 
-/// <reference path="../../star.ts" />
-
-
 import Star from "../../Star";
 
 
@@ -13,7 +10,7 @@ interface PropTypes extends React.Props<any>
   isHighlighted: boolean;
   usedCapacity: number;
   totalCapacity: number;
-  onClick: reactTypeTODO_func;
+  onClick: (star: Star) => void;
 }
 
 interface StateType
@@ -46,33 +43,28 @@ export class ManufactoryStarsListItemComponent extends React.Component<PropTypes
 
   render()
   {
-    var star = this.props.star;
-    var isHighlighted = this.props.isHighlighted;
-    var usedCapacity = this.props.usedCapacity;
-    var totalCapacity = this.props.totalCapacity;
-
-    var hasManufcatory = Boolean(totalCapacity);
-    var hasCapacity = hasManufcatory && usedCapacity < totalCapacity;
+    var hasManufactory = Boolean(this.props.totalCapacity);
+    var hasCapacity = hasManufactory && this.props.usedCapacity < this.props.totalCapacity;
 
     return(
       React.DOM.div(
       {
         className: "manufactory-stars-list-item" +
-          (!hasManufcatory ? " no-manufactory" : "") +
-          (isHighlighted ? " highlighted" : ""),
+          (!hasManufactory ? " no-manufactory" : "") +
+          (this.props.isHighlighted ? " highlighted" : ""),
         onClick: this.handleClick
       },
         React.DOM.div(
         {
           className: "manufactory-stars-list-item-star-name"
         },
-          star.name
+          this.props.star.name
         ),
-        !hasManufcatory ? null : React.DOM.div(
+        !hasManufactory ? null : React.DOM.div(
         {
           className: "manufactory-stars-list-item-capacity" + (!hasCapacity ? " no-capacity" : "")
         },
-          "" + usedCapacity + "/" + totalCapacity
+          "" + this.props.usedCapacity + "/" + this.props.totalCapacity
         )
       )
     );
