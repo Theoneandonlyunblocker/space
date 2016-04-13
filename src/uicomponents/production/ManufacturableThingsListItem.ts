a6f4320a86c1dec99993c8b0eff9b742b4b90ed6
giraluna <giraluna3@gmail.com>
2016-04-13 17:42:45 +0300

    manufacturablethingslistitem

--- src/uicomponents/production/ManufacturableThingsListItem.ts
+++ src/uicomponents/production/ManufacturableThingsListItem.ts
@@ -1,19 +1,19 @@
 /// <reference path="../../../lib/react-0.13.3.d.ts" />
 import * as React from "react";
 
+import ManufacturableThing from "../../templateinterfaces/ManufacturableThing";
+
 interface PropTypes extends React.Props<any>
 {
-  template: reactTypeTODO_any;
+  template: ManufacturableThing;
   parentIndex: number;
-  onClick?: reactTypeTODO_func;
+  onClick?: (template: ManufacturableThing, parentIndex: number) => void;
   showCost: boolean;
   money?: number;
 }
 
 interface StateType
 {
-  canAfford?: boolean;
-  isDisabled?: boolean;
 }
 
 export class ManufacturableThingsListItemComponent extends React.Component<PropTypes, StateType>
@@ -65,12 +65,8 @@ export class ManufacturableThingsListItemComponent extends React.Component<PropT
 
   render()
   {
-    var template = this.props.template;
-    var isDisabled: boolean = this.state.isDisabled;
-    if (this.props.showCost)
-    {
-      isDisabled = isDisabled || !this.state.canAfford;
-    }
+    const canAfford = this.props.money >= this.props.template.buildCost;
+    const isDisabled = !Boolean(this.props.onClick) || (this.props.showCost && !canAfford);
 
     return(
       React.DOM.li(
@@ -78,20 +74,20 @@ export class ManufacturableThingsListItemComponent extends React.Component<PropT
         className: "manufacturable-things-list-item" + (isDisabled ? " disabled" : ""),
         onClick: (isDisabled ? null : this.handleClick),
         disabled: isDisabled,
-        title: template.description
+        title: this.props.template.description
       },
         React.DOM.div(
         {
           className: "manufacturable-things-list-item-name"
         },
-          template.displayName
+          this.props.template.displayName
         ),
         !this.props.showCost ? null : React.DOM.div(
         {
           className: "manufacturable-things-list-item-cost money-style" +
-            (this.state.canAfford ? "" : " negative")
+            (canAfford ? "" : " negative")
         },
-          template.buildCost
+          this.props.template.buildCost
         )
       )
     );
