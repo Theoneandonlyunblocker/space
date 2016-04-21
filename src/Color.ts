952d57367be0923c5325e710d8d241fe3e3a5d9b
giraluna <giraluna3@gmail.com>
2016-04-21 10:40:49 +0300

    hoist function declaration in Color.fromHSL as required by strict mode

--- src/Color.ts
+++ src/Color.ts
@@ -59,6 +59,15 @@ export default class Color
   public static fromHSL(h: number, s: number, l: number): Color
   {
     let r: number, g: number, b: number;
+    function hue2rgb(p: number, q: number, t: number): number
+    {
+      if(t < 0) t += 1;
+      if(t > 1) t -= 1;
+      if(t < 1/6) return p + (q - p) * 6 * t;
+      if(t < 1/2) return q;
+      if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
 
     if (s == 0)
     {
@@ -66,16 +75,6 @@ export default class Color
     }
     else
     {
-      function hue2rgb(p: number, q: number, t: number): number
-      {
-        if(t < 0) t += 1;
-        if(t > 1) t -= 1;
-        if(t < 1/6) return p + (q - p) * 6 * t;
-        if(t < 1/2) return q;
-        if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-        return p;
-      }
-
       const q: number = l < 0.5 ? l * (1 + s) : l + s - l * s;
       const p: number = 2 * l - q;
       r = hue2rgb(p, q, h + 1/3);
