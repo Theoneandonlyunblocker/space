0def7e4054bccb98d635408757e70b40c3f00d0b
giraluna <giraluna3@gmail.com>
2016-04-26 12:36:02 +0300

    Add separate component for save deletion confirm text

---
+++ src/uicomponents/saves/ConfirmDeleteSavesContent.ts
@@ -0,0 +1,60 @@
+/// <reference path="../../../lib/react-global.d.ts" />
+
+interface PropTypes extends React.Props<any>
+{
+  saveNames: string[];
+}
+
+interface StateType
+{
+}
+
+export class ConfirmDeleteSavesContentComponent extends React.Component<PropTypes, StateType>
+{
+  displayName = "ConfirmDeleteSavesContent";
+  state: StateType;
+  
+  constructor(props: PropTypes)
+  {
+    super(props);
+  }
+  
+  shouldComponentUpdate = React.addons.PureRenderMixin.shouldComponentUpdate.bind(this);
+  
+  render()
+  {
+    return(
+      React.DOM.div(
+      {
+        className: "confirm-delete-saves-content"
+      },
+        React.DOM.span(
+        {
+          className: "confirm-delete-saves-content-title"
+        },
+          "Are you sure you want to delete the following saves?"
+        ),
+        React.DOM.ol(
+        {
+          className: "confirm-delete-saves-content-saves-list"
+        },
+          this.props.saveNames.map(saveName =>
+          {
+            return(
+              React.DOM.li(
+              {
+                className: "confirm-delete-saves-content-save-name",
+                key: saveName
+              },
+                saveName
+              )
+            );
+          })
+        )
+      )
+    );
+  }
+}
+
+const Factory: React.Factory<PropTypes> = React.createFactory(ConfirmDeleteSavesContentComponent);
+export default Factory;
