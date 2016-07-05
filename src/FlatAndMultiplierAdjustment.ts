7e6a123cff20b597b72c3b765436e5847c498bfd
giraluna <giraluna3@gmail.com>
2016-07-05 11:53:55 +0300

    Correct typing for FlatAndMultiplierAdjustment interface

--- src/FlatAndMultiplierAdjustment.ts
+++ src/FlatAndMultiplierAdjustment.ts
@@ -1,7 +1,20 @@
-declare interface FlatAndMultiplierAdjustment
+// https://github.com/Microsoft/TypeScript/issues/7485
+// declare interface FlatAndMultiplierAdjustment
+// {
+//   flat?: number;
+//   multiplier?: number;
+// }
+
+interface FlatAdjustment
 {
-  flat?: number;
-  multiplier?: number;
+  flat: number;
 }
+
+interface MultiplierAdjustment
+{
+  multiplier: number;
+}
+
+declare type FlatAndMultiplierAdjustment = FlatAdjustment | MultiplierAdjustment;
 
 export default FlatAndMultiplierAdjustment;
