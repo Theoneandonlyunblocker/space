bb3032b1e2631010a776e08d5980565ed98f9f57
giraluna <giraluna3@gmail.com>
2016-09-21 10:22:01 +0300

    Create base class for deriving sfx fragment prop editor components consisting of two numbers

--- src/uicomponents/sfxeditor/props/Point.ts
+++ src/uicomponents/sfxeditor/props/Point.ts
@@ -2,6 +2,8 @@
 
 import SFXFragment from "../../../../modules/common/battlesfxfunctions/sfxfragments/SFXFragment";
 
+import Vec2Base from "./Vec2Base";
+
 interface PropTypes extends React.Props<any>
 {
   propName: string;
@@ -12,98 +14,19 @@ interface PropTypes extends React.Props<any>
   y: number;
 }
 
-interface StateType
-{
-}
-
-export class SFXFragmentPropPointComponent extends React.Component<PropTypes, StateType>
+export class SFXFragmentPropPointComponent extends Vec2Base<PropTypes>
 {
   displayName = "SFXFragmentPropPoint";
-  state: StateType;
   
+  value1Key = "x";
+  value2Key = "y";
+
+  value1Label = "X";
+  value2Label = "Y";
+
   constructor(props: PropTypes)
   {
     super(props);
-
-    this.handleXChange = this.handleValueChange.bind(this, "x");
-    this.handleYChange = this.handleValueChange.bind(this, "y");
-  }
-
-  private handleXChange: (e: React.FormEvent) => void;
-  private handleYChange: (e: React.FormEvent) => void;
-  private handleValueChange(key: "x" | "y", e: React.FormEvent): void
-  {
-    const target = <HTMLInputElement> e.target;
-    const value = parseFloat(target.value);
-
-    const valueIsValid = isFinite(value);
-
-    if (!valueIsValid)
-    {
-      return;
-    }
-
-    this.props.fragment.props[this.props.propName][key] = value;
-
-    this.props.onValueChange();
-  }
-  
-  render()
-  {
-    const baseID = "sfx-fragment-prop-point-value-" + this.props.propName;
-    return(
-      React.DOM.div(
-      {
-        className: "sfx-fragment-prop-point-wrapper"
-      },
-        React.DOM.div(
-        {
-          className: "sfx-fragment-prop-point-value-wrapper"
-        },
-          React.DOM.label(
-          {
-            className: "sfx-fragment-prop-point-label",
-            htmlFor: baseID + "x"
-          },
-            "X:",
-          ),
-          React.DOM.input(
-          {
-            className: "sfx-fragment-prop-point-input",
-            id: baseID + "x",
-            type: "number",
-            onChange: this.handleXChange,
-            step: 0.1,
-            value: "" + this.props.x
-          },
-            
-          )
-        ),
-        React.DOM.div(
-        {
-          className: "sfx-fragment-prop-point-value-wrapper"
-        },
-          React.DOM.label(
-          {
-            className: "sfx-fragment-prop-point-label",
-            htmlFor: baseID + "y"
-          },
-            "Y:",
-          ),
-          React.DOM.input(
-          {
-            className: "sfx-fragment-prop-point-input",
-            id: baseID + "y",
-            type: "number",
-            onChange: this.handleYChange,
-            step: 0.1,
-            value: "" + this.props.y
-          },
-            
-          )
-        )
-      )
-    );
   }
 }
 
