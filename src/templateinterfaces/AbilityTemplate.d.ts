daf88f3d2723c0578b9ada9f9483759ba199aeff
giraluna <giraluna3@gmail.com>
2016-09-26 11:35:32 +0300

    Use jsdoc style comments for ability template declaration file
    helps intellisense

--- src/templateinterfaces/AbilityTemplate.d.ts
+++ src/templateinterfaces/AbilityTemplate.d.ts
@@ -6,7 +6,6 @@ import
   GetBattleTargetsFN
 } from "../targeting";
 
-// TODO 25.9.2016 | use jsdoc comments for intellisense
 declare interface AbilityTemplate extends AbilityBase
 {
   type: string;
@@ -17,13 +16,18 @@ declare interface AbilityTemplate extends AbilityBase
   {
     turnsToPrep: number;
     interruptsNeeded: number;
-    // moveDelay is used for turns in which ability is used or being prepared
+    /**
+     * Delay added for each turn spent preparing. ability use turn included
+     * moveDelay is used when preparation is complete and effects are executed
+     */
     prepDelay: number;
   };
   actionsUse: number;
   // if not true, user will lose all guard before using ability
+  // TODO 26.9.2016 | rename
   preservesUserGuard?: boolean;
   // if true, ability will always hit intended target. guard can still provide defensive benefits
+  // TODO 26.9.2016 | rename
   bypassesGuard?: boolean;
   
   getPossibleTargets: GetBattleTargetsFN;
@@ -36,20 +40,23 @@ declare interface AbilityTemplate extends AbilityBase
   beforeUse?: AbilityEffectTemplate[];
   afterUse?: AbilityEffectTemplate[];
   
-  // how likely the AI will consider using this ability relative to other available ones
-  // doesn't affect AI's final decision on which ability to use, but can guide it
-  // in the right direction
-  AIEvaluationPriority?: number; // default = 1
-  // adjusts the final score of this ability. AI picks move with highest score.
-  // used to penalize moves that might be optimal but boring, such as doing nothing
-  AIScoreAdjust?: number;
-  // prevent from being used in AI vs AI battles. helps when simulation depth is too low
-  // to let AIScoreAdjust kick in
   /**
-   * @description Test
+   * how likely the AI will consider using this ability relative to other available ones
+   * doesn't affect AI's final decision on which ability to use, but can guide it
+   * in the right direction
+   * default = 1
+   */
+  AIEvaluationPriority?: number;
+  /**
+   * adjusts the final score of this ability. AI picks move with highest score.
+   * used to penalize moves that might be optimal but boring, such as doing nothing
+   */
+  AIScoreAdjust?: number;
+  /**
+   * prevent from being used in AI vs AI battles. helps when simulation depth is too low
+   * to let AIScoreAdjust kick in
    */
   disableInAIBattles?: boolean;
-  
 }
 
 export default AbilityTemplate;
