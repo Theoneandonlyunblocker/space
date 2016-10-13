178b2fde640d8c0df6a21d60e4ede65a75495ac6
giraluna <giraluna3@gmail.com>
2016-10-13 11:46:42 +0300

    Remove unused properties from sub emblem templates

--- modules/defaultemblems/SubEmblemTemplates.ts
+++ modules/defaultemblems/SubEmblemTemplates.ts
@@ -5,148 +5,106 @@ export const Aguila_explayada_2: SubEmblemTemplate =
 {
   key: "Aguila_explayada_2",
   src: "modules/defaultemblems/img/Aguila_explayada_2.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Berliner_Baer: SubEmblemTemplate =
 {
   key: "Berliner_Baer",
   src: "modules/defaultemblems/img/Berliner_Baer.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Cles_en_sautoir: SubEmblemTemplate =
 {
   key: "Cles_en_sautoir",
   src: "modules/defaultemblems/img/Cles_en_sautoir.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Coa_Illustration_Cross_Bowen_3: SubEmblemTemplate =
 {
   key: "Coa_Illustration_Cross_Bowen_3",
   src: "modules/defaultemblems/img/Coa_Illustration_Cross_Bowen_3.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Coa_Illustration_Cross_Malte_1: SubEmblemTemplate =
 {
   key: "Coa_Illustration_Cross_Malte_1",
   src: "modules/defaultemblems/img/Coa_Illustration_Cross_Malte_1.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Coa_Illustration_Elements_Planet_Moon: SubEmblemTemplate =
 {
   key: "Coa_Illustration_Elements_Planet_Moon",
   src: "modules/defaultemblems/img/Coa_Illustration_Elements_Planet_Moon.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Couronne_heraldique_svg: SubEmblemTemplate =
 {
   key: "Couronne_heraldique_svg",
   src: "modules/defaultemblems/img/Couronne_heraldique_svg.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Gomaisasa: SubEmblemTemplate =
 {
   key: "Gomaisasa",
   src: "modules/defaultemblems/img/Gomaisasa.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Gryphon_Segreant: SubEmblemTemplate =
 {
   key: "Gryphon_Segreant",
   src: "modules/defaultemblems/img/Gryphon_Segreant.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Heraldic_pentacle: SubEmblemTemplate =
 {
   key: "Heraldic_pentacle",
   src: "modules/defaultemblems/img/Heraldic_pentacle.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Japanese_Crest_Futatsudomoe_1: SubEmblemTemplate =
 {
   key: "Japanese_Crest_Futatsudomoe_1",
   src: "modules/defaultemblems/img/Japanese_Crest_Futatsudomoe_1.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Japanese_Crest_Hana_Hisi: SubEmblemTemplate =
 {
   key: "Japanese_Crest_Hana_Hisi",
   src: "modules/defaultemblems/img/Japanese_Crest_Hana_Hisi.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Japanese_Crest_Mitsumori_Janome: SubEmblemTemplate =
 {
   key: "Japanese_Crest_Mitsumori_Janome",
   src: "modules/defaultemblems/img/Japanese_Crest_Mitsumori_Janome.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Japanese_Crest_Oda_ka: SubEmblemTemplate =
 {
   key: "Japanese_Crest_Oda_ka",
   src: "modules/defaultemblems/img/Japanese_Crest_Oda_ka.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Japanese_crest_Tsuki_ni_Hoshi: SubEmblemTemplate =
 {
   key: "Japanese_crest_Tsuki_ni_Hoshi",
   src: "modules/defaultemblems/img/Japanese_crest_Tsuki_ni_Hoshi.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Japanese_Crest_Ume: SubEmblemTemplate =
 {
   key: "Japanese_Crest_Ume",
   src: "modules/defaultemblems/img/Japanese_Crest_Ume.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Mitsuuroko: SubEmblemTemplate =
 {
   key: "Mitsuuroko",
   src: "modules/defaultemblems/img/Mitsuuroko.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Musubikashiwa: SubEmblemTemplate =
 {
   key: "Musubikashiwa",
   src: "modules/defaultemblems/img/Musubi-kashiwa.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Takeda_mon: SubEmblemTemplate =
 {
   key: "Takeda_mon",
   src: "modules/defaultemblems/img/Takeda_mon.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const threeHorns: SubEmblemTemplate =
 {
   key: "threeHorns",
   src: "modules/defaultemblems/img/threeHorns.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
 }
 export const Flag_of_Edward_England: SubEmblemTemplate =
 {
   key: "Flag_of_Edward_England",
   src: "modules/defaultemblems/img/Flag_of_Edward_England.svg",
-  minAmountOfColors: 1,
-  maxAmountOfColors: 1,
   disallowRandomGeneration: true,
 }
 
