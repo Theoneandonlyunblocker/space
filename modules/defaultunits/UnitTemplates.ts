ddfa878cb3bef31d99ec6d35811ebf8e20b91ea2
giraluna <giraluna3@gmail.com>
2016-10-19 19:49:33 +0300

    Remove 'blueShip' and 'redShip' templates
    Were used for testing old unit distribution system

--- modules/defaultunits/UnitTemplates.ts
+++ modules/defaultunits/UnitTemplates.ts
@@ -3,9 +3,7 @@ import UnitTemplate from "../../src/templateinterfaces/UnitTemplate";
 
 import battleCruiser from  "./templates/battleCruiser";
 import commandShip from  "./templates/commandShip";
-import redShip from  "./templates/redShip";
 import stealthShip from  "./templates/stealthShip";
-import blueShip from  "./templates/blueShip";
 import debugShip from  "./templates/debugShip";
 import scout from  "./templates/scout";
 import bomberSquadron from  "./templates/bomberSquadron";
@@ -16,9 +14,7 @@ const UnitTemplates: TemplateCollection<UnitTemplate> =
 {
   [battleCruiser.type]: battleCruiser,
   [commandShip.type]: commandShip,
-  [redShip.type]: redShip,
   [stealthShip.type]: stealthShip,
-  [blueShip.type]: blueShip,
   [debugShip.type]: debugShip,
   [scout.type]: scout,
   [bomberSquadron.type]: bomberSquadron,
