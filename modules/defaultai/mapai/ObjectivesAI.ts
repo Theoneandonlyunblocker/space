dbbc88087378d1cc2b16bfd102739f5c153d6fe8
giraluna <giraluna3@gmail.com>
2017-02-25 21:27:36 +0200

    remove old objectivesai class

--- modules/defaultai/mapai/ObjectivesAI.ts
+++ modules/defaultai/mapai/ObjectivesAI.ts
@@ -1,15 +1,4 @@
-import GrandStrategyAI from "./GrandStrategyAI";
-import MapEvaluator from "./MapEvaluator";
-import Objective from "./Objective";
-
-import GalaxyMap from "../../../src/GalaxyMap";
-import Player from "../../../src/Player";
-
-import allObjectiveTemplates from "../Objectives";
-import RoutineAdjustmentByID from "../RoutineAdjustmentByID";
-import ObjectiveTemplate from "../objectives/common/ObjectiveTemplate";
-
-/*
+/* OLD STUFF
 -- objectives ai
 get expansion targets
 create expansion objectives with priority based on score
@@ -38,147 +27,42 @@ scouting objectives
     track enemy armies
   perimeter
     create perimeter of vision around own locations
- */
+END OLD STUFF*/
 
-export default class ObjectivesAI
+/*
+diplo           |
+front requests  V
+econ
+front moves
+diplo
+econ
+*/
+
+import {DiplomaticObjective} from "../objectives/common/DiplomaticObjective";
+import {EconomicObjective} from "../objectives/common/EconomicObjective";
+import {FrontObjective} from "../objectives/common/FrontObjective";
+
+export class ObjectivesAI
 {
-  mapEvaluator: MapEvaluator;
-  map: GalaxyMap;
-  player: Player;
-  grandStrategyAI: GrandStrategyAI;
+  private diplomaticObjectives: DiplomaticObjective[];
+  private economicObjectives: EconomicObjective[];
+  private frontObjectives: FrontObjective[];
 
-  objectivesByType:
+  public processDiplomaticObjectives(): void
   {
-    [objectiveType: string]: Objective[];
-  } = {};
-  objectives: Objective[] = [];
 
-  requests: any[] = [];
-
-  constructor(mapEvaluator: MapEvaluator, grandStrategyAI: GrandStrategyAI)
-  {
-    this.mapEvaluator = mapEvaluator;
-    this.map = mapEvaluator.map;
-    this.player = mapEvaluator.player;
-    this.grandStrategyAI = grandStrategyAI;
   }
-  clearObjectives()
+  public processEconomicObjectives(): void
   {
-    this.objectives = [];
-  }
-  setAllDiplomaticObjectives()
-  {
-    this.clearObjectives();
-    this.setAllObjectivesWithTemplateProperty("diplomacyRoutineFN");
-  }
-  setAllEconomicObjectives()
-  {
-    this.clearObjectives();
-    this.setAllObjectivesWithTemplateProperty("economyRoutineFN");
-  }
-  setAllMoveObjectives()
-  {
-    this.clearObjectives();
-    this.setAllObjectivesWithTemplateProperty("moveRoutineFN");
-  }
-  setAllObjectivesWithTemplateProperty(propKey: string)
-  {
-    for (let key in allObjectiveTemplates)
-    {
-      const template = allObjectiveTemplates[key];
-      if (template[propKey])
-      {
-        this.setObjectivesOfType(allObjectiveTemplates[key]);
-      }
-    }
-  }
-  getNewObjectivesOfType(objectiveTemplate: ObjectiveTemplate)
-  {
-    const objectiveType = objectiveTemplate.key;
-    const byTarget = this.getObjectivesByTarget(objectiveType, true);
-    const newObjectives = objectiveTemplate.creatorFunction(this.grandStrategyAI, this.mapEvaluator, this);
-    const finalObjectives: Objective[] = [];
 
-    for (let i = 0; i < newObjectives.length; i++)
-    {
-      const newObjective = newObjectives[i];
-      if (newObjective.priority < 0.04)
-      {
-        continue;
-      }
-      const keyString = newObjective.target ? newObjective.target.id : "noTarget";
-      const oldObjective = byTarget[keyString];
-      if (oldObjective)
-      {
-        oldObjective.priority = newObjective.priority;
-        finalObjectives.push(oldObjective);
-      }
-      else
-      {
-        finalObjectives.push(newObjective);
-      }
-    }
-
-    return finalObjectives;
   }
-  setObjectivesOfType(objectiveTemplate: ObjectiveTemplate)
+  public createFrontRequests(): void
   {
-    const newObjectives = this.getNewObjectivesOfType(objectiveTemplate);
-    this.objectivesByType[objectiveTemplate.key] = newObjectives;
-    this.objectives = this.objectives.concat(newObjectives);
+
   }
-  getObjectivesByTarget(objectiveType: string, markAsOngoing: boolean)
+  public executeFrontMoves(): void
   {
-    const objectivesByTarget:
-    {
-      [targetString: string]: Objective;
-    } = {};
 
-    if (!this.objectivesByType[objectiveType])
-    {
-      return objectivesByTarget;
-    }
-
-    for (let i = 0; i < this.objectivesByType[objectiveType].length; i++)
-    {
-      const objective = this.objectivesByType[objectiveType][i];
-      if (markAsOngoing) objective.isOngoing = true;
-
-      const keyString = objective.target ? objective.target.id : "noTarget";
-      objectivesByTarget[keyString] = objective;
-    }
-
-    return objectivesByTarget;
-  }
-  getObjectivesWithTemplateProperty(propKey: string)
-  {
-    return this.objectives.filter(function(objective: Objective)
-    {
-      return Boolean(objective.template[propKey]);
-    });
-  }
-  getAdjustmentsForTemplateProperty(propKey: string)
-  {
-    const withAdjustment = this.getObjectivesWithTemplateProperty(propKey);
-    const adjustments: RoutineAdjustmentByID = {};
-
-    for (let i = 0; i < withAdjustment.length; i++)
-    {
-      for (let j = 0; j < withAdjustment[i].template[propKey].length; j++)
-      {
-        const adjustment = withAdjustment[i].template[propKey][j];
-        if (!adjustments[adjustment.target.id])
-        {
-          adjustments[adjustment.target.id] =
-          {
-            target: adjustment.target,
-            multiplier: 1,
-          };
-        }
-        adjustments[adjustment.target.id].multiplier += adjustment.multiplier;
-      }
-    }
-
-    return adjustments;
   }
 }
+
