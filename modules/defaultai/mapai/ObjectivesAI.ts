e75a2e3dd0cc8526fe2c8b93201aa0ba928aee5a
giraluna <giraluna3@gmail.com>
2017-03-27 12:25:39 +0300

    start working on new oai

--- modules/defaultai/mapai/ObjectivesAI.ts
+++ modules/defaultai/mapai/ObjectivesAI.ts
@@ -38,19 +38,51 @@ diplo
 econ
 */
 
-import {DiplomaticObjective} from "../objectives/common/DiplomaticObjective";
-import {EconomicObjective} from "../objectives/common/EconomicObjective";
-import {FrontObjective} from "../objectives/common/FrontObjective";
+import {GrandStrategyAI} from "./GrandStrategyAI";
+import MapEvaluator from "./MapEvaluator";
+
+import {Objective} from "../objectives/common/Objective";
+import {ObjectiveCreatorTemplate} from "../objectives/common/ObjectiveCreatorTemplate";
+import {ObjectiveFamily} from "../objectives/common/ObjectiveFamily";
+
+import IDDictionary from "../../../src/IDDictionary";
+import
+{
+  getRelativeValue,
+} from "../../../src/utility";
 
 export class ObjectivesAI
 {
-  private diplomaticObjectives: DiplomaticObjective[];
-  private economicObjectives: EconomicObjective[];
-  private frontObjectives: FrontObjective[];
+  private readonly objectiveCreatorTemplates: ObjectiveCreatorTemplate[] = [];
+  private ongoingObjectives: Objective[] = [];
+
+  private readonly grandStrategyAI: GrandStrategyAI;
+  private readonly mapEvaluator: MapEvaluator;
 
   public processDiplomaticObjectives(): void
   {
+    const filterFN = (toFilter: Objective | ObjectiveCreatorTemplate) =>
+    {
+      return toFilter.family === ObjectiveFamily.Diplomatic;
+    };
 
+    // update objectives
+    this.updateObjectivesForFilter(filterFN);
+
+    // evaluate priorities
+    const priorities: {[type: string]: number} = {};
+    this.objectiveCreatorTemplates.filter(filterFN).forEach(template =>
+    {
+      priorities[template.type] = template.evaluatePriority(this.mapEvaluator, this.grandStrategyAI);
+    });
+    // calculate relative scores
+    let minScore: number;
+    let maxScore: number;
+
+
+    // final priority = priority * relative score
+
+    // execute
   }
   public processEconomicObjectives(): void
   {
@@ -64,5 +96,60 @@ export class ObjectivesAI
   {
 
   }
-}
 
+  /**
+   * removes from this.ongoingObjectives as a side effect
+   */
+  private spliceOngoingObjectives(filterFN: (objective: Objective) => boolean): Objective[]
+  {
+    const filteredOngoingObjectives: Objective[] = [];
+    const splicedObjectives: Objective[] = [];
+
+    this.ongoingObjectives.forEach(objective =>
+    {
+      if (filterFN(objective))
+      {
+        splicedObjectives.push(objective);
+      }
+      else
+      {
+        filteredOngoingObjectives.push(objective);
+      }
+    });
+
+    this.ongoingObjectives = filteredOngoingObjectives;
+    return splicedObjectives;
+  }
+  private updateObjectivesForFilter(
+    filterFN: (toFilter: Objective | ObjectiveCreatorTemplate) => boolean,
+  ): void
+  {
+    const creatorTemplates = this.objectiveCreatorTemplates.filter(filterFN);
+
+    const ongoingObjectives = this.spliceOngoingObjectives(filterFN);
+
+    creatorTemplates.forEach(template =>
+    {
+      const newObjectives = template.getObjectives(this.mapEvaluator, ongoingObjectives);
+      this.ongoingObjectives.push(...newObjectives);
+    });
+  }
+  private getRelativeScoresForObjectives(objectives: Objective[]): IDDictionary<Objective, number>
+  {
+    let min: number;
+    let max: number;
+
+    objectives.forEach(objective =>
+    {
+      const score = objective.score;
+
+      min = isFinite(min) ? Math.min(min, score) : score;
+      max = isFinite(max) ? Math.max(max, score) : score;
+    });
+
+    return new IDDictionary(objectives, objective =>
+    {
+      return getRelativeValue(objective.score, min, max);
+    });
+  }
+}
