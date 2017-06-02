eab888a82a86b81b31eea810364729db4646ac8c
giraluna <giraluna3@gmail.com>
2017-06-02 10:35:20 +0300

    define type for exported localization function for default ai

--- modules/defaultai/localization/localize.ts
+++ modules/defaultai/localization/localize.ts
@@ -4,4 +4,4 @@ import {Localizer} from "../../../src/localization/Localizer";
 
 const localizer = new Localizer<typeof tradeMessages>();
 
-export const localize = localizer.localize.bind(localizer);
+export const localize: typeof localizer.localize = localizer.localize.bind(localizer);
