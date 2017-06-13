409c9dba44d91fae532141e04158ff4f68830d7d
giraluna <giraluna3@gmail.com>
2017-06-13 16:15:04 +0300

    add default languages for built-in modules

---
+++ modules/common/defaultLanguages.ts
@@ -0,0 +1,13 @@
+import {Language} from "../../src/localization/Language";
+
+export const en: Language =
+{
+  code: "en",
+  displayName: "English",
+};
+
+export const ja: Language =
+{
+  code: "ja",
+  displayName: "日本語",
+};
