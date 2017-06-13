ad3e4beb81a218c11140357d264b4a76ae791446
giraluna <giraluna3@gmail.com>
2017-06-13 16:18:33 +0300

    add additional fields to language objects for ui purposes

--- src/localization/Language.ts
+++ src/localization/Language.ts
@@ -3,6 +3,8 @@ export interface Language
   // try and use ISO 639-1 if possible
   // http://www.loc.gov/standards/iso639-2/php/English_list.php
   code: string;
+  displayName: string;
+  flagSrc?: string;
 
   // getSubjectPronoun(characters: ...Character[]): string;
   // getObjectPronoun(characters: ...Character[]): string;
