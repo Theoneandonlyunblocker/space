28b506ad6f4b3120bc3a309a0df6c05116366840
giraluna <giraluna3@gmail.com>
2017-07-07 09:55:19 +0300

    new window system for notification filter button

--- src/uicomponents/notifications/NotificationFilterButton.ts
+++ src/uicomponents/notifications/NotificationFilterButton.ts
@@ -4,8 +4,7 @@ import NotificationFilter from "../../NotificationFilter";
 
 import {Language} from "../../localization/Language";
 
-// import {default as PopupManager, PopupManagerComponent} from "../popups/PopupManager";
-// import TopMenuPopup from "../popups/TopMenuPopup";
+import {default as DefaultWindow} from "../windows/DefaultWindow";
 
 import NotificationFilterList from "./NotificationFilterList";
 
@@ -20,95 +19,27 @@ export interface PropTypes extends React.Props<any>
 
 interface StateType
 {
-  notificationFilterPopup?: number;
+  hasNotificationFilterPopup?: boolean;
 }
 
 export class NotificationFilterButtonComponent extends React.Component<PropTypes, StateType>
 {
-  displayName: string = "NotificationFilterButton";
-
-  state: StateType;
-  popupManager: PopupManagerComponent;
+  public displayName: string = "NotificationFilterButton";
+  public state: StateType;
 
   constructor(props: PropTypes)
   {
     super(props);
 
-    this.state = this.getInitialStateTODO();
+    this.state =
+    {
+      hasNotificationFilterPopup: false,
+    };
 
     this.bindMethods();
   }
-  private bindMethods()
-  {
-    this.makePopup = this.makePopup.bind(this);
-    this.closePopup = this.closePopup.bind(this);
-    this.togglePopup = this.togglePopup.bind(this);
-  }
 
-  private getInitialStateTODO(): StateType
-  {
-    return(
-    {
-      notificationFilterPopup: undefined,
-    });
-  }
-
-  makePopup()
-  {
-    const popupId = this.popupManager.makePopup(
-    {
-      content: TopMenuPopup(
-      {
-        content: NotificationFilterList(
-        {
-          filter: this.props.filter,
-          highlightedOptionKey: this.props.highlightedOptionKey,
-          activeLanguage: this.props.activeLanguage,
-        }),
-        handleClose: this.closePopup,
-        title: "Message settings",
-      }),
-      popupProps:
-      {
-        dragPositionerProps:
-        {
-          startOnHandleElementOnly: true,
-          preventAutoResize: true,
-        },
-        resizable: true,
-        minWidth: 440,
-        minHeight: 150,
-      },
-    });
-
-    this.setState(
-    {
-      notificationFilterPopup: popupId,
-    });
-  }
-
-  closePopup()
-  {
-    this.popupManager.closePopup(this.state.notificationFilterPopup);
-    this.setState(
-    {
-      notificationFilterPopup: undefined,
-    });
-  }
-
-  togglePopup()
-  {
-    if (isFinite(this.state.notificationFilterPopup))
-    {
-      this.closePopup();
-    }
-    else
-    {
-      this.makePopup();
-    }
-  }
-
-  render()
+  public render()
   {
     return(
       React.DOM.div(
@@ -122,16 +53,56 @@ export class NotificationFilterButtonComponent extends React.Component<PropTypes
         },
           this.props.text,
         ),
-        PopupManager(
-        {
-          ref: (component: PopupManagerComponent) =>
+        !this.state.hasNotificationFilterPopup ? null :
+          DefaultWindow(
           {
-            this.popupManager = component;
+            title: "Message settings",
+            handleClose: this.closePopup,
+
+            minWidth: 440,
+            minHeight: 150,
           },
-          onlyAllowOne: true,
-        }),
+            NotificationFilterList(
+            {
+              filter: this.props.filter,
+              highlightedOptionKey: this.props.highlightedOptionKey,
+              activeLanguage: this.props.activeLanguage,
+            }),
+          ),
       )
     );
+  }
+
+  private bindMethods()
+  {
+    this.openPopup = this.openPopup.bind(this);
+    this.closePopup = this.closePopup.bind(this);
+    this.togglePopup = this.togglePopup.bind(this);
+  }
+  private openPopup(): void
+  {
+    this.setState(
+    {
+      hasNotificationFilterPopup: true,
+    });
+  }
+  private closePopup(): void
+  {
+    this.setState(
+    {
+      hasNotificationFilterPopup: false,
+    });
+  }
+  private togglePopup(): void
+  {
+    if (this.state.hasNotificationFilterPopup)
+    {
+      this.closePopup();
+    }
+    else
+    {
+      this.openPopup();
+    }
   }
 }
 
