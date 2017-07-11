101e635ed0cdb0073b8476975226421845cf79a9
giraluna <giraluna3@gmail.com>
2017-07-11 18:40:45 +0300

    fix tslint warnings for color picker component

--- src/uicomponents/setupgame/ColorPicker.ts
+++ src/uicomponents/setupgame/ColorPicker.ts
@@ -2,7 +2,9 @@
 
 import Color from "../../Color";
 
+import NumberInput from "../generic/NumberInput";
 import NumericTextInput from "../generic/NumericTextInput";
+
 import
 {
   AutoPositionerProps,
@@ -31,8 +33,8 @@ interface StateType
 
 export class ColorPickerComponent extends React.Component<PropTypes, StateType>
 {
-  displayName: string = "ColorPicker";
-  state: StateType;
+  public displayName: string = "ColorPicker";
+  public state: StateType;
 
   private onChangeTimeoutHandle: number = null;
   private baseElementID: string = "color-picker";
@@ -51,241 +53,8 @@ export class ColorPickerComponent extends React.Component<PropTypes, StateType>
 
     this.bindMethods();
   }
-  private bindMethods()
-  {
-    this.setHue = this.setHue.bind(this);
-    this.setSat = this.setSat.bind(this);
-    this.makeHsvInputs = this.makeHsvInputs.bind(this);
-    this.autoGenerateColor = this.autoGenerateColor.bind(this);
-    this.updateFromHex = this.updateFromHex.bind(this);
-    this.updateFromHsv = this.updateFromHsv.bind(this);
-    this.makeGradientStyle = this.makeGradientStyle.bind(this);
-    this.triggerParentOnChange = this.triggerParentOnChange.bind(this);
-    this.setVal = this.setVal.bind(this);
-    this.makeGradientString = this.makeGradientString.bind(this);
-    this.nullifyColor = this.nullifyColor.bind(this);
-  }
 
-  private getStateFromColor(color: Color): StateType
-  {
-    const hsvColor = Color.convertScalarsToDegrees(color.getHSV());
-
-    return(
-    {
-      hexColor: color.getHex(),
-      hue: hsvColor[0],
-      sat: hsvColor[1],
-      val: hsvColor[2],
-    });
-  }
-
-  triggerParentOnChange(color: Color, isNull: boolean)
-  {
-    if (this.onChangeTimeoutHandle)
-    {
-      window.clearTimeout(this.onChangeTimeoutHandle);
-      this.onChangeTimeoutHandle = null;
-    }
-
-    if (this.props.minUpdateBuffer)
-    {
-      this.onChangeTimeoutHandle = window.setTimeout(() =>
-      {
-      }, this.props.minUpdateBuffer || 0);
-    }
-    else
-    {
-      this.props.onChange(color, isNull);
-    }
-  }
-
-  updateFromHsv(hue: number, sat: number, val: number, e?: Event)
-  {
-    if (e && e.type !== "change")
-    {
-      return;
-    }
-
-    const color: Color = Color.fromHSV.apply(null, Color.convertDegreesToScalars([hue, sat, val]));
-
-    this.setState(
-    {
-      hexColor: color.getHex(),
-    });
-
-    if (this.props.onChange)
-    {
-      this.triggerParentOnChange(color, false);
-    }
-  }
-  updateFromHex(hexColor: number)
-  {
-    const color = Color.fromHex(hexColor);
-    const hsvColor = Color.convertScalarsToDegrees(color.getHSV());
-
-    this.setState(
-    {
-      hue: Math.round(hsvColor[0]),
-      sat: Math.round(hsvColor[1]),
-      val: Math.round(hsvColor[2]),
-    });
-
-    if (this.props.onChange)
-    {
-      this.triggerParentOnChange(color, false);
-    }
-  }
-  setHue(e: Event)
-  {
-    const target = <HTMLInputElement> e.target;
-    let hue = Math.round(parseInt(target.value) % 361);
-    if (hue < 0) hue = 360;
-    this.setState({hue: hue});
-    this.updateFromHsv(hue, this.state.sat, this.state.val, e);
-  }
-  setSat(e: Event)
-  {
-    const target = <HTMLInputElement> e.target;
-    let sat = Math.round(parseInt(target.value) % 101);
-    if (sat < 0) sat = 100;
-    this.setState({sat: sat});
-    this.updateFromHsv(this.state.hue, sat, this.state.val, e);
-  }
-  setVal(e: Event)
-  {
-    const target = <HTMLInputElement> e.target;
-    let val = Math.round(parseInt(target.value) % 101);
-    if (val < 0) val = 100;
-    this.setState({val: val});
-    this.updateFromHsv(this.state.hue, this.state.sat, val, e);
-  }
-
-  autoGenerateColor()
-  {
-    const color = this.props.generateColor();
-    const hexColor = color.getHex();
-
-    this.setState(
-    {
-      hexColor: hexColor,
-    });
-
-    this.updateFromHex(hexColor);
-  }
-
-  nullifyColor()
-  {
-    if (this.props.onChange)
-    {
-      this.triggerParentOnChange(Color.fromHex(this.state.hexColor), true);
-    }
-  }
-
-  makeGradientString(min: string, max: string)
-  {
-    return(
-      "linear-gradient(to right, " +
-      min + " 0%, " +
-      max + " 100%)"
-    );
-  }
-
-  private makeHexStringFromHSVDegreeArray(hsv: number[])
-  {
-    const color: Color = Color.fromHSV.apply(null, Color.convertDegreesToScalars(hsv));
-    return color.getHexString();
-  }
-
-  makeGradientStyle(type: string): React.CSSProperties
-  {
-    const hue = this.state.hue;
-    const sat = this.state.sat;
-    const val = this.state.val;
-
-    switch (type)
-    {
-      case "hue":
-      {
-        return(
-        {
-
-        });
-      }
-      case "sat":
-      {
-        const min = "#" + this.makeHexStringFromHSVDegreeArray([hue, 0, val]);
-        const max = "#" + this.makeHexStringFromHSVDegreeArray([hue, 100, val]);
-        return(
-        {
-          background: this.makeGradientString(min, max),
-        });
-      }
-      case "val":
-      {
-        const min = "#" + this.makeHexStringFromHSVDegreeArray([hue, sat, 0]);
-        const max = "#" + this.makeHexStringFromHSVDegreeArray([hue, sat, 100]);
-        return(
-        {
-          background: this.makeGradientString(min, max),
-        });
-      }
-      default:
-      {
-        return null;
-      }
-    }
-  }
-
-  makeHsvInputs(type: string)
-  {
-    const label = "" + type[0].toUpperCase() + ":";
-
-    const max = type === "hue" ? 360 : 100;
-    const updateFunctions =
-    {
-      hue: this.setHue,
-      sat: this.setSat,
-      val: this.setVal,
-    };
-
-    const idForType = "" + this.baseElementID + "-" + type;
-
-    return(
-      React.DOM.div({className: "color-picker-input-container", key: type},
-        React.DOM.label({className: "color-picker-label", htmlFor: idForType}, label),
-        React.DOM.div(
-        {
-          className: "color-picker-slider-background" + " color-picker-slider-background-" + type,
-          style: this.makeGradientStyle(type),
-        },
-          React.DOM.input(
-          {
-            className: "color-picker-slider",
-            id: idForType,
-            ref: type,
-            type: "range",
-            min: 0,
-            max: max,
-            step: 1,
-            value: this.state[type],
-            onChange: updateFunctions[type],
-            onMouseUp: updateFunctions[type],
-            onTouchEnd: updateFunctions[type],
-          }),
-        ),
-        // TODO 2017.07.10 | use NumericTextInput component for all these
-        React.DOM.input(
-        {
-          className: "color-picker-input",
-          type: "number",
-          step: 1,
-          value: "" + Math.round(this.state[type]),
-          onChange: updateFunctions[type],
-        }),
-      )
-    );
-  }
-  render()
+  public render()
   {
     return(
       React.DOM.div({className: "color-picker"},
@@ -338,6 +107,244 @@ export class ColorPickerComponent extends React.Component<PropTypes, StateType>
             },
           }),
         ),
+      )
+    );
+  }
+
+  private bindMethods()
+  {
+    this.setHue = this.setHue.bind(this);
+    this.setSat = this.setSat.bind(this);
+    this.makeHsvInputs = this.makeHsvInputs.bind(this);
+    this.autoGenerateColor = this.autoGenerateColor.bind(this);
+    this.updateFromHex = this.updateFromHex.bind(this);
+    this.updateFromHsv = this.updateFromHsv.bind(this);
+    this.makeGradientStyle = this.makeGradientStyle.bind(this);
+    this.triggerParentOnChange = this.triggerParentOnChange.bind(this);
+    this.setVal = this.setVal.bind(this);
+    this.makeGradientString = this.makeGradientString.bind(this);
+    this.nullifyColor = this.nullifyColor.bind(this);
+  }
+  private getStateFromColor(color: Color): StateType
+  {
+    const hsvColor = Color.convertScalarsToDegrees(color.getHSV());
+
+    return(
+    {
+      hexColor: color.getHex(),
+      hue: hsvColor[0],
+      sat: hsvColor[1],
+      val: hsvColor[2],
+    });
+  }
+  private triggerParentOnChange(color: Color, isNull: boolean)
+  {
+    if (this.onChangeTimeoutHandle)
+    {
+      window.clearTimeout(this.onChangeTimeoutHandle);
+      this.onChangeTimeoutHandle = null;
+    }
+
+    if (this.props.minUpdateBuffer)
+    {
+      this.onChangeTimeoutHandle = window.setTimeout(() =>
+      {
+      }, this.props.minUpdateBuffer || 0);
+    }
+    else
+    {
+      this.props.onChange(color, isNull);
+    }
+  }
+  private updateFromHsv(hue: number, sat: number, val: number, e?: Event)
+  {
+    if (e && e.type !== "change")
+    {
+      return;
+    }
+
+    const color: Color = Color.fromHSV.apply(null, Color.convertDegreesToScalars([hue, sat, val]));
+
+    this.setState(
+    {
+      hexColor: color.getHex(),
+    });
+
+    if (this.props.onChange)
+    {
+      this.triggerParentOnChange(color, false);
+    }
+  }
+  private updateFromHex(hexColor: number)
+  {
+    const color = Color.fromHex(hexColor);
+    const hsvColor = Color.convertScalarsToDegrees(color.getHSV());
+
+    this.setState(
+    {
+      hue: Math.round(hsvColor[0]),
+      sat: Math.round(hsvColor[1]),
+      val: Math.round(hsvColor[2]),
+    });
+
+    if (this.props.onChange)
+    {
+      this.triggerParentOnChange(color, false);
+    }
+  }
+  private setHue(e: Event)
+  {
+    const target = <HTMLInputElement> e.target;
+    let hue = Math.round(parseInt(target.value) % 361);
+    if (hue < 0)
+    {
+      hue = 360;
+    }
+    this.setState({hue: hue});
+    this.updateFromHsv(hue, this.state.sat, this.state.val, e);
+  }
+  private setSat(e: Event)
+  {
+    const target = <HTMLInputElement> e.target;
+    let sat = Math.round(parseInt(target.value) % 101);
+    if (sat < 0)
+    {
+      sat = 100;
+    }
+    this.setState({sat: sat});
+    this.updateFromHsv(this.state.hue, sat, this.state.val, e);
+  }
+  private setVal(e: Event)
+  {
+    const target = <HTMLInputElement> e.target;
+    let val = Math.round(parseInt(target.value) % 101);
+    if (val < 0)
+    {
+      val = 100;
+    }
+    this.setState({val: val});
+    this.updateFromHsv(this.state.hue, this.state.sat, val, e);
+  }
+  private autoGenerateColor()
+  {
+    const color = this.props.generateColor();
+    const hexColor = color.getHex();
+
+    this.setState(
+    {
+      hexColor: hexColor,
+    });
+
+    this.updateFromHex(hexColor);
+  }
+  private nullifyColor()
+  {
+    if (this.props.onChange)
+    {
+      this.triggerParentOnChange(Color.fromHex(this.state.hexColor), true);
+    }
+  }
+  private makeGradientString(min: string, max: string)
+  {
+    return(
+      "linear-gradient(to right, " +
+      min + " 0%, " +
+      max + " 100%)"
+    );
+  }
+  private makeHexStringFromHSVDegreeArray(hsv: number[])
+  {
+    const color: Color = Color.fromHSV.apply(null, Color.convertDegreesToScalars(hsv));
+
+    return color.getHexString();
+  }
+  private makeGradientStyle(type: string): React.CSSProperties
+  {
+    const hue = this.state.hue;
+    const sat = this.state.sat;
+    const val = this.state.val;
+
+    switch (type)
+    {
+      case "hue":
+      {
+        return(
+        {
+
+        });
+      }
+      case "sat":
+      {
+        const min = "#" + this.makeHexStringFromHSVDegreeArray([hue, 0, val]);
+        const max = "#" + this.makeHexStringFromHSVDegreeArray([hue, 100, val]);
+
+        return(
+        {
+          background: this.makeGradientString(min, max),
+        });
+      }
+      case "val":
+      {
+        const min = "#" + this.makeHexStringFromHSVDegreeArray([hue, sat, 0]);
+        const max = "#" + this.makeHexStringFromHSVDegreeArray([hue, sat, 100]);
+
+        return(
+        {
+          background: this.makeGradientString(min, max),
+        });
+      }
+      default:
+      {
+        return null;
+      }
+    }
+  }
+  private makeHsvInputs(type: string)
+  {
+    const label = "" + type[0].toUpperCase() + ":";
+
+    const max = type === "hue" ? 360 : 100;
+    const updateFunctions =
+    {
+      hue: this.setHue,
+      sat: this.setSat,
+      val: this.setVal,
+    };
+
+    const idForType = "" + this.baseElementID + "-" + type;
+
+    return(
+      React.DOM.div({className: "color-picker-input-container", key: type},
+        React.DOM.label({className: "color-picker-label", htmlFor: idForType}, label),
+        React.DOM.div(
+        {
+          className: "color-picker-slider-background" + " color-picker-slider-background-" + type,
+          style: this.makeGradientStyle(type),
+        },
+          React.DOM.input(
+          {
+            className: "color-picker-slider",
+            id: idForType,
+            ref: type,
+            type: "range",
+            min: 0,
+            max: max,
+            step: 1,
+            value: this.state[type],
+            onChange: updateFunctions[type],
+            onMouseUp: updateFunctions[type],
+            onTouchEnd: updateFunctions[type],
+          }),
+        ),
+        // TODO 2017.07.10 | use NumericTextInput component for all these
+        React.DOM.input(
+        {
+          className: "color-picker-input",
+          type: "number",
+          step: 1,
+          value: "" + Math.round(this.state[type]),
+          onChange: updateFunctions[type],
+        }),
       )
     );
   }
