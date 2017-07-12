eb7acfb4b913cdc6d46238b9afbb9154ce735824
giraluna <giraluna3@gmail.com>
2017-07-12 12:02:57 +0300

    continuously adjust value if spinner arrows are held down

--- src/uicomponents/generic/Spinner.ts
+++ src/uicomponents/generic/Spinner.ts
@@ -1,5 +1,7 @@
 /// <reference path="../../../lib/react-global.d.ts" />
 
+import {FixedRateTicker} from "../../FixedRateTicker";
+
 import
 {
   clamp,
@@ -25,14 +27,31 @@ export class SpinnerComponent extends React.Component<PropTypes, StateType>
   public displayName = "Spinner";
   public state: StateType;
 
+  private spinDirection: 1 | -1;
+  private startSpinTimeoutHandle: number;
+
+  private readonly ticker: FixedRateTicker;
+  private readonly timeBetweenSpins = 50;
+  private readonly spinStartDelay = 300;
+
   constructor(props: PropTypes)
   {
     super(props);
 
     this.stepUp = this.stepUp.bind(this);
     this.stepDown = this.stepDown.bind(this);
+    this.startSpinUp = this.startSpinUp.bind(this);
+    this.startSpinDown = this.startSpinDown.bind(this);
+    this.stopSpin = this.stopSpin.bind(this);
+    this.onSpinTick = this.onSpinTick.bind(this);
+
+    this.ticker = new FixedRateTicker(this.onSpinTick, this.timeBetweenSpins);
   }
 
+  public componentWillUnmount(): void
+  {
+    this.stopSpin();
+  }
   public render()
   {
     return(
@@ -44,11 +63,15 @@ export class SpinnerComponent extends React.Component<PropTypes, StateType>
         {
           className: "spinner-arrow spinner-arrow-up",
           onClick: this.stepUp,
+          onMouseDown: this.startSpinUp,
+          onTouchStart: this.startSpinUp,
         }),
         React.DOM.button(
         {
           className: "spinner-arrow spinner-arrow-down",
           onClick: this.stepDown,
+          onMouseDown: this.startSpinDown,
+          onTouchStart: this.startSpinDown,
         }),
       )
     );
@@ -74,6 +97,45 @@ export class SpinnerComponent extends React.Component<PropTypes, StateType>
   private stepDown(): void
   {
     this.adjust(-this.props.step);
+  }
+  private startSpinUp(): void
+  {
+    this.spinDirection = 1;
+
+    this.startSpin();
+  }
+  private startSpinDown(): void
+  {
+    this.spinDirection = -1;
+
+    this.startSpin();
+  }
+  private startSpin(): void
+  {
+    document.addEventListener("mouseup", this.stopSpin);
+    document.addEventListener("touchend", this.stopSpin);
+
+    this.startSpinTimeoutHandle = window.setTimeout(() =>
+    {
+      this.ticker.start();
+    }, this.spinStartDelay);
+  }
+  private stopSpin(): void
+  {
+    this.ticker.stop();
+
+    if (isFinite(this.startSpinTimeoutHandle))
+    {
+      window.clearTimeout(this.startSpinTimeoutHandle);
+    }
+    this.startSpinTimeoutHandle = undefined;
+
+    document.removeEventListener("mouseup", this.stopSpin);
+    document.removeEventListener("touchend", this.stopSpin);
+  }
+  private onSpinTick(ticks: number): void
+  {
+    this.adjust(this.spinDirection * this.props.step * ticks);
   }
 }
 
