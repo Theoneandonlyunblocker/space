3a0a8c5706180cfcc5362eba63640ad9066e6c46
giraluna <giraluna3@gmail.com>
2017-07-12 22:18:55 +0300

    remove duplicate call for DragPositioner.onDragMove when dragging starts

--- src/uicomponents/mixins/DragPositioner.ts
+++ src/uicomponents/mixins/DragPositioner.ts
@@ -262,11 +262,6 @@ export default class DragPositioner<T extends React.Component<any, any>> impleme
         {
           this.onDragStart(x, y);
         }
-        // TODO 2017.07.04 | can't we delete this? called below in this.handleDrag()
-        if (this.onDragMove)
-        {
-          this.onDragMove(x, y);
-        }
       }
     }
 
