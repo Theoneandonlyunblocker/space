fe9712333aadc30cc0c5767f78f3acb16a9075fa
giraluna <giraluna3@gmail.com>
2017-07-12 11:59:55 +0300

    create ticker class for triggering updates at fixed intervals

---
+++ src/FixedRateTicker.ts
@@ -0,0 +1,45 @@
+/// <reference path="../lib/pixi.d.ts" />
+
+export class FixedRateTicker
+{
+  public tickRate: number;
+
+  private readonly ticker: PIXI.ticker.Ticker;
+  private readonly onTick: (tickCount: number) => void;
+
+  private accumulatedTime: number = 0;
+  private startTime: number;
+
+  constructor(onTick: (tickCount: number) => void, tickRate: number)
+  {
+    this.tickRate = tickRate;
+    this.onTick = onTick;
+
+    this.ticker = new PIXI.ticker.Ticker();
+    this.ticker.add(this.onTickerUpdate, this);
+  }
+
+  public start(): void
+  {
+    this.accumulatedTime = 0;
+    this.startTime = performance.now();
+    this.ticker.start();
+  }
+  public stop(): void
+  {
+    this.ticker.stop();
+  }
+
+  private onTickerUpdate(): void
+  {
+    this.accumulatedTime += this.ticker.elapsedMS;
+
+    const ticksToPlay = Math.floor(this.accumulatedTime / this.tickRate);
+
+    if (ticksToPlay)
+    {
+      this.accumulatedTime -= ticksToPlay * this.tickRate;
+      this.onTick(ticksToPlay);
+    }
+  }
+}
