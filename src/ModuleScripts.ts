ed2be3a41b7badc15e72e0ae493c5d5446ad3a40
giraluna <giraluna3@gmail.com>
2017-07-27 11:17:50 +0300

    use getters instead of explicit .get() method for accessing module scripts

--- src/ModuleScripts.ts
+++ src/ModuleScripts.ts
@@ -4,46 +4,89 @@ import {GameScripts} from "./modulescriptinterfaces/GameScripts";
 import {PlayerScripts} from "./modulescriptinterfaces/PlayerScripts";
 import {UnitScripts} from "./modulescriptinterfaces/UnitScripts";
 
-import {ModuleScriptData} from "./ModuleScriptData";
+import {ModuleScriptData, ScriptsWithData} from "./ModuleScriptData";
 
 
-type ScriptsWithData<Scripts extends {[T in keyof Scripts]: (...args: any[]) => void}> =
+type AllModuleScriptTypes =
 {
-  [T in keyof Scripts]: ModuleScriptData<Scripts[T]>[];
+  battle: BattleScripts;
+  diplomacy: DiplomacyScripts;
+  game: GameScripts;
+  player: PlayerScripts;
+  unit: UnitScripts;
 };
 
-interface AllModuleScriptsWithData
+// why doesn't this work directly?
+type _dummy_<ScriptTypes extends
 {
-  battle: ScriptsWithData<BattleScripts>;
-  diplomacy: ScriptsWithData<DiplomacyScripts>;
-  game: ScriptsWithData<GameScripts>;
-  player: ScriptsWithData<PlayerScripts>;
-  unit: ScriptsWithData<UnitScripts>;
-}
+  [C in keyof ScriptTypes]:
+  {
+    [K in keyof ScriptTypes[C]]: (...args: any[]) => void;
+  }
+}> =
+{
+  [C in keyof ScriptTypes]: ScriptsWithData<ScriptTypes[C]>;
+};
+
+type InnerPartial<T> =
+{
+  [K in keyof T]: Partial<T[K]>;
+};
+
+
+
+type AllModuleScriptsWithData = _dummy_<AllModuleScriptTypes>;
+
+type PartialModuleScriptsWithData = Partial<InnerPartial<AllModuleScriptsWithData>>;
+
+type ScriptsCollection<S extends {[C in keyof S]: (...args: any[]) => void}> =
+{
+  [C in keyof S]: S[C][];
+};
+
+type AllModuleScripts =
+{
+  [C in keyof AllModuleScriptTypes]:
+  {
+    [K in keyof AllModuleScriptTypes[C]]: AllModuleScriptTypes[C][K][];
+  }
+};
 
 // TODO 2017.06.13 | move non-internal stuff from eventManager to here
-export default class ModuleScripts implements AllModuleScriptsWithData
+export default class ModuleScripts implements AllModuleScripts
 {
-  public readonly battle: ScriptsWithData<BattleScripts> =
+  /* tslint:disable:member-ordering */
+  private readonly scriptsWithData: AllModuleScriptsWithData =
   {
-    battleFinish: [],
+    battle:
+    {
+      battleFinish: [],
+    },
+    diplomacy:
+    {
+      onWarDeclaration: [],
+    },
+    game:
+    {
+      afterInit: [],
+    },
+    player:
+    {
+      onDeath: [],
+    },
+    unit:
+    {
+      removeFromPlayer: [],
+      onCapture: [],
+    },
   };
-  public readonly diplomacy: ScriptsWithData<DiplomacyScripts> =
-  {
-    onWarDeclaration: [],
-  };
-  public readonly game: ScriptsWithData<GameScripts> =
-  {
-    afterInit: [],
-  };
-  public readonly player: ScriptsWithData<PlayerScripts> =
-  {
-    onDeath: [],
-  };
-  public readonly unit: ScriptsWithData<UnitScripts> =
-  {
-    removeFromPlayer: [],
-  };
+
+  public readonly battle    : ScriptsCollection<BattleScripts>    = ModuleScripts.makeAccessorObject(this.scriptsWithData.battle);
+  public readonly diplomacy : ScriptsCollection<DiplomacyScripts> = ModuleScripts.makeAccessorObject(this.scriptsWithData.diplomacy);
+  public readonly game      : ScriptsCollection<GameScripts>      = ModuleScripts.makeAccessorObject(this.scriptsWithData.game);
+  public readonly player    : ScriptsCollection<PlayerScripts>    = ModuleScripts.makeAccessorObject(this.scriptsWithData.player);
+  public readonly unit      : ScriptsCollection<UnitScripts>      = ModuleScripts.makeAccessorObject(this.scriptsWithData.unit);
+  /* tslint:enable:member-ordering */
 
   constructor()
   {
@@ -56,7 +99,7 @@ export default class ModuleScripts implements AllModuleScriptsWithData
 
     toMerge.forEach(moduleScripts =>
     {
-      merged.add(moduleScripts);
+      merged.add(moduleScripts.scriptsWithData);
     });
 
     return merged;
@@ -65,49 +108,46 @@ export default class ModuleScripts implements AllModuleScriptsWithData
   {
     return b.priority - a.priority;
   }
+  private static makeAccessorObject<S extends {[T in keyof S]: (...args: any[]) => void}>(scriptsWithData: ScriptsWithData<S>): ScriptsCollection<S>
+  {
+    const accessorObject = <ScriptsCollection<S>> {};
 
-  public add(...allScriptData: Partial<AllModuleScriptsWithData>[]): void
+    for (let scriptKey in scriptsWithData)
+    {
+      Object.defineProperty(accessorObject, scriptKey,
+      {
+        get: () => scriptsWithData[scriptKey].sort(ModuleScripts.sort).map(scriptData => scriptData.script),
+      });
+    }
+
+    return accessorObject;
+  }
+
+  public add(...allScriptData: PartialModuleScriptsWithData[]): void
   {
     allScriptData.forEach(toAdd =>
     {
       for (let category in toAdd)
       {
-        for (let scriptType in toAdd[category])
+        for (let scriptKey in toAdd[category])
         {
-          this[category][scriptType].push(...toAdd[category][scriptType]);
+          this.scriptsWithData[category][scriptKey].push(...toAdd[category][scriptKey]);
         }
       }
     });
   }
-  public remove(toRemove: Partial<AllModuleScriptsWithData>): void
+  public remove(toRemove: PartialModuleScriptsWithData): void
   {
     for (let category in toRemove)
     {
-      for (let scriptType in toRemove[category])
+      for (let scriptKey in toRemove[category])
       {
-        const ownScripts: Function[] = this[category][scriptType];
-        const scriptsToRemove: Function[] = toRemove[category][scriptType];
-
-        for (let j = 0; j < scriptsToRemove.length; j++)
+        this.scriptsWithData[category][scriptKey] =
+          this.scriptsWithData[category][scriptKey].filter((scriptData: ModuleScriptData<any>) =>
         {
-          for (let i = ownScripts.length - 1; i >= 0; i--)
-          {
-            if (ownScripts[i] === scriptsToRemove[j])
-            {
-              ownScripts.splice(i, 1);
-
-              break;
-            }
-          }
-        }
+          return toRemove[category][scriptKey].indexOf(scriptData) !== -1;
+        });
       }
     }
-  }
-  public get<T extends (...args: any[]) => void>(allScriptData: ModuleScriptData<T>[]): T[]
-  {
-    return allScriptData.sort(ModuleScripts.sort).map(scriptData =>
-    {
-      return scriptData.script;
-    });
   }
 }
