47c7cb01a4dd7a6b9c3fd1dcb89fc78dc6410977
giraluna <giraluna3@gmail.com>
2017-08-05 12:26:34 +0300

    register trade message localization to localization function

--- modules/defaultai/localization/localize.ts
+++ modules/defaultai/localization/localize.ts
@@ -2,6 +2,11 @@ import {tradeMessages} from "./en/tradeMessages";
 
 import {Localizer} from "../../../src/localization/Localizer";
 
+import * as Languages from "../../../localization/defaultLanguages";
+
+
 export const localizer = new Localizer<typeof tradeMessages>("tradeMessages");
+
+localizer.registerTexts(tradeMessages, Languages.en);
 
 export const localize: typeof localizer.localize = localizer.localize.bind(localizer);
