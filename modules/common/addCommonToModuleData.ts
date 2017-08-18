82d212a6c36b7a16acf3f1de3bd832d4a5cd0db6
giraluna <giraluna3@gmail.com>
2017-08-18 15:14:19 +0300

    copy terrain templates to module data

--- modules/common/addCommonToModuleData.ts
+++ modules/common/addCommonToModuleData.ts
@@ -4,6 +4,7 @@ import * as AbilityTemplates from  "./abilitytemplates/abilities";
 import * as BattleSFXTemplates from  "./battlesfxtemplates/battleSFX";
 import * as PassiveSkillTemplates from  "./passiveskilltemplates/passiveSkills";
 import * as ResourceTemplates from  "./resourcetemplates/resources";
+import * as TerrainTemplates from  "./terraintemplates/terrains";
 import {unitEffectTemplates} from  "./uniteffecttemplates/unitEffectTemplates";
 
 import {attachedUnitDataScripts} from "./attachedUnitData";
@@ -15,7 +16,7 @@ export default function addCommonToModuleData(moduleData: ModuleData)
   moduleData.copyTemplates<any>(ResourceTemplates, "Resources");
   moduleData.copyTemplates<any>(BattleSFXTemplates, "BattleSFX");
   moduleData.copyTemplates<any>(PassiveSkillTemplates, "PassiveSkills");
-
+  moduleData.copyTemplates<any>(TerrainTemplates, "Terrains");
   moduleData.copyTemplates<any>(unitEffectTemplates, "UnitEffects");
 
   moduleData.scripts.add(attachedUnitDataScripts);
