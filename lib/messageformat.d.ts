d61a937add7072c6b46717ca1af6ee2141ecc7c8
giraluna <giraluna3@gmail.com>
2017-11-16 15:38:34 +0200

    definitions for messageformat.js

---
+++ lib/messageformat.d.ts
@@ -0,0 +1,11 @@
+declare type MessageFunction<P> = (props: P) => string;
+
+declare class MessageFormat
+{
+  // actually string | Array.<string> | Object.<string, function()>	<optional>
+  constructor(locale: string);
+
+  public addFormatters(formatters: {[key: string]: (toFormat: string, ...args: any[]) => string}): void;
+  // has other forms as well
+  public compile<P>(message: string): MessageFunction<P>;
+}
