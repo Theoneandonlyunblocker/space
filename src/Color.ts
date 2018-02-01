dc1e007fda688a2eeb98594d0d8a291d232dead4
giraluna <giraluna3@gmail.com>
2018-02-01 17:19:53 +0200

    bump tslint ver up to 5.8.x

--- src/Color.ts
+++ src/Color.ts
@@ -52,12 +52,14 @@ export default class Color
     t = v * (1 - (1 - f) * s);
     switch (i % 6)
     {
+      // tslint:disable:ban-comma-operator
       case 0: r = v, g = t, b = p; break;
       case 1: r = q, g = v, b = p; break;
       case 2: r = p, g = v, b = t; break;
       case 3: r = p, g = q, b = v; break;
       case 4: r = t, g = p, b = v; break;
       case 5: r = v, g = p, b = q; break;
+      // tslint:enable:ban-comma-operator
     }
     return new Color(r, g, b);
   }
