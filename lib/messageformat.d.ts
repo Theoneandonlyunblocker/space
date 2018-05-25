d49c81bb34192c4c3e4f3d9016f379ea6428c822
giraluna <giraluna3@gmail.com>
2018-05-25 11:13:51 +0300

    more accurate definition for messageFormat custom formatter functions

--- lib/messageformat.d.ts
+++ lib/messageformat.d.ts
@@ -1,11 +1,13 @@
+// actually string | Array.<string> | Object.<string, function()>	<optional>
+declare type Locale = string;
 declare type MessageFunction<T> = (...args: T[]) => string;
+declare type FormatterFunction = (value: any, currentLocale?: Locale, arg?: string) => string;
 
 declare class MessageFormat
 {
-  // actually string | Array.<string> | Object.<string, function()>	<optional>
-  constructor(locale: string);
+  constructor(locale: Locale);
 
-  public addFormatters(formatters: {[key: string]: (toFormat: string, ...args: any[]) => string}): void;
+  public addFormatters(formatters: {[key: string]: FormatterFunction}): void;
   // has other forms as well
   public compile<P>(message: string): MessageFunction<P>;
 }
