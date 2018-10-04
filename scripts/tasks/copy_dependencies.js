5bc88205786a2c7579066ff8571fd6fc4adf4437
giraluna <giraluna3@gmail.com>
2018-10-04 17:31:12 +0300

    don't try to copy nonexistent react-transition-group lib

--- scripts/tasks/copy_dependencies.js
+++ scripts/tasks/copy_dependencies.js
@@ -10,8 +10,7 @@ const sources =
 [
   "node_modules/pixi.js/dist/pixi.js",
   "node_modules/react/dist/react-with-addons.js",
-  "node_modules/react-dom/dist/react-dom.js",
-  "node_modules/react-transition-group/dist/react-transition-group.js"
+  "node_modules/react-dom/dist/react-dom.js"
 ];
 
 sources.forEach(source =>
