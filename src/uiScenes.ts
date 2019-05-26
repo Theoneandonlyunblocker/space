3dec717654d95fc54822d0a91585406cf9ee37eb
giraluna <giraluna3@gmail.com>
2019-05-26 10:01:12 +0300

    move ReactUIScene.ts => uiScenes.ts
    to prep for decoupling ui from core

---
+++ src/uiScenes.ts
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+
+// TODO 2019.05.26 | should be "React.ComponentFactory<PropTypes, IComponent>" i think for type checking
+// tslint:disable:no-any
+type UIScenes = {
+  battle: React.Factory<any>,
+  battlePrep: React.Factory<any>,
+  galaxyMap: React.Factory<any>,
+  setupGame: React.Factory<any>,
+  errorRecovery: React.Factory<any>,
+
+  // debug
+  flagMaker: React.Factory<any>;
+  // sfxEditor: React.Factory<any>;
+  battleSceneTester: React.Factory<any>;
+};
+// tslint:enable:no-any
+
+export const uiScenes: Partial<UIScenes> = {};
+
+export type ReactUIScene = keyof UIScenes;
