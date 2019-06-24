c8e67e1398329b1e6c6d68e6bd05fce501a295a9
giraluna <giraluna3@gmail.com>
2019-06-24 11:11:23 +0300

    add python script for prefixing json imports in built AMD files

---
+++ scripts/prefix_json_amd_imports.py
@@ -0,0 +1,35 @@
+import os
+import re
+
+def processFile(inFile, fileName):
+  rewrittenLines = []
+  hasMatch = False
+
+  inFile.seek(0, 0)
+  for line in inFile:
+    replacedLine = line
+
+    if "./moduleInfo.json" in line:
+      replacedLine = line.replace("./moduleInfo.json", "json!./moduleInfo.json")
+      hasMatch = True
+
+    rewrittenLines.append(replacedLine)
+
+  return (hasMatch, rewrittenLines)
+
+matchesFound = 0
+
+for root, dirs, files in os.walk("./projectDist/modules/", topdown=False):
+  for fileName in files:
+    if fileName.endswith(".js"):
+      hasMatch = False
+      with open(os.path.join(root, fileName), "r") as inFile:
+        hasMatch, reWrittenLines = processFile(inFile, fileName)
+      if hasMatch:
+        matchesFound += 1
+        with open(os.path.join(root, fileName), "w") as outFile:
+          for line in reWrittenLines:
+            outFile.write(line)
+
+print("Prefixed JSON import in {} files\n".format(matchesFound))
+
