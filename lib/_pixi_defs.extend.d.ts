941dc49a1c30d9ea1a2d7662d8a8e9bb87ef7437
giraluna <giraluna3@gmail.com>
2019-07-01 22:03:27 +0300

    add definition extensions for pixi

---
+++ lib/_pixi_defs.extend.d.ts
@@ -0,0 +1,17 @@
+declare namespace PIXI {
+  interface Container {
+    destroy(options: boolean |
+    {
+      children?: boolean;
+      texture?: boolean;
+      baseTexture?: boolean;
+    }): void;
+  }
+  interface Graphics {
+    currentPath: PIXI.Polygon;
+  }
+  class Filter<U extends object> {
+    constructor(vertexSrc?: string, fragmentSrc?: string, uniforms?: U);
+    uniforms: U;
+  }
+}
