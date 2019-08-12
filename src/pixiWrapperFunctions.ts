decff6c2ff3573ba013768ad602ebea99b8df59b
giraluna <giraluna3@gmail.com>
2019-08-12 12:44:23 +0300

    use translationMatrix for TilingRope
    allows scaling etc.

--- src/pixiWrapperFunctions.ts
+++ src/pixiWrapperFunctions.ts
@@ -188,7 +188,22 @@ export class TilingRope extends PIXI.Mesh
 
     const geometry = new PIXI.RopeGeometry(texture.height, points);
     const shader = PIXI.Shader.from(
-    undefined,
+    `
+    precision mediump float;
+
+    attribute vec2 aVertexPosition;
+    attribute vec2 aTextureCoord;
+
+    uniform mat3 translationMatrix;
+    uniform mat3 projectionMatrix;
+
+    varying vec2 vTextureCoord;
+
+    void main() {
+      vTextureCoord = aTextureCoord;
+      gl_Position = vec4((projectionMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+    }
+    `,
     `
     varying vec2 vTextureCoord;
 
