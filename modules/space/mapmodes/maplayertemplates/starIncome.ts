ac4b0814b3726049ed3692936a347557a9f8a5eb
giraluna <giraluna3@gmail.com>
2019-08-21 14:34:06 +0300

    apply income layer alpha to layer itself rather than drawn objects

--- modules/space/mapmodes/maplayertemplates/starIncome.ts
+++ modules/space/mapmodes/maplayertemplates/starIncome.ts
@@ -14,6 +14,7 @@ export const starIncome: MapRendererLayerTemplate =
 {
   key: "starIncome",
   displayName: "Income",
+  initialAlpha: 0.6,
   interactive: false,
   isUsedForCameraBounds: true,
   drawingFunction: (map, perspectivePlayer) =>
@@ -63,7 +64,7 @@ export const starIncome: MapRendererLayerTemplate =
 
       const poly = makePolygonFromPoints(star.voronoiCell.vertices);
       const gfx = new PIXI.Graphics();
-      gfx.beginFill(color, 0.6);
+      gfx.beginFill(color, 1);
       gfx.drawShape(poly);
       gfx.endFill();
       doc.addChild(gfx);
