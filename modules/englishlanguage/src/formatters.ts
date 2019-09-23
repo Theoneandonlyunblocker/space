c25a12b6d4b3231e23ad2ab813b763a4fd292af4
giraluna <giraluna3@gmail.com>
2019-09-23 22:55:30 +0300

    allow chaining capitalize message formatter

--- modules/englishlanguage/src/formatters.ts
+++ modules/englishlanguage/src/formatters.ts
@@ -3,7 +3,19 @@ import { EnglishName } from "./EnglishName";
 
 export const formatters =
 {
-  capitalize: (s: string) => s.charAt(0).toUpperCase() + s.slice(1),
+  capitalize: (s: string, locale: string, formatterOutput?: string) =>
+  {
+    // example: "_, capitalize, {name, possessiveName}"
+    if (formatterOutput)
+    {
+      return formatterOutput.charAt(0).toUpperCase() + formatterOutput.slice(1);
+    }
+    // example "{displayName, capitalize}"
+    else
+    {
+      return s.charAt(0).toUpperCase() + s.slice(1);
+    }
+  },
   possessiveName: (name: EnglishName) =>
   {
     const lastChar = name.baseName[name.baseName.length - 1];
