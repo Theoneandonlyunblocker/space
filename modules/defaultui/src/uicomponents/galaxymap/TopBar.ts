1536820868f344006d3046eaae77fc4774db089c
giraluna <giraluna3@gmail.com>
2019-10-04 14:17:12 +0300

    remove unnecessary listeners from TopBar ui component
    its children should know to update when resources/income changes

--- modules/defaultui/src/uicomponents/galaxymap/TopBar.ts
+++ modules/defaultui/src/uicomponents/galaxymap/TopBar.ts
@@ -4,7 +4,6 @@ import * as ReactDOMElements from "react-dom-factories";
 import {localize} from "../../../localization/localize";
 import {Game} from "core/src/game/Game";
 import {Player} from "core/src/player/Player";
-import {eventManager} from "core/src/app/eventManager";
 import {PlayerFlag} from "../PlayerFlag";
 
 import { PlayerResourcesWithIncome } from "../resources/PlayerResourcesWithIncome";
@@ -23,10 +22,6 @@ interface StateType
 export class TopBarComponent extends React.Component<PropTypes, StateType>
 {
   public displayName = "TopBar";
-  private readonly updateListeners:
-  {
-    [key: string]: () => void;
-  } = {};
 
   public state: StateType;
 
@@ -34,25 +29,6 @@ export class TopBarComponent extends React.Component<PropTypes, StateType>
   {
     super(props);
   }
-
-  componentDidMount()
-  {
-    this.updateListeners["builtBuildingWithEffect_income"] =
-      eventManager.addEventListener("builtBuildingWithEffect_income", this.forceUpdate.bind(this));
-    this.updateListeners["builtBuildingWithEffect_resourceIncome"] =
-      eventManager.addEventListener("builtBuildingWithEffect_resourceIncome", this.forceUpdate.bind(this));
-    this.updateListeners["playerMoneyUpdated"] =
-      eventManager.addEventListener("playerMoneyUpdated", this.forceUpdate.bind(this));
-  }
-
-  componentWillUnmount()
-  {
-    for (const key in this.updateListeners)
-    {
-      eventManager.removeEventListener(key, this.updateListeners[key]);
-    }
-  }
-
   render()
   {
     const player = this.props.player;
