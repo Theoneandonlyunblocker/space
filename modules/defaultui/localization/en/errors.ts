4a173b8f37d1eb3bcdc8955e373e202f17255e6a
giraluna <giraluna3@gmail.com>
2019-12-21 19:57:34 +0200

    clarify message when displaying ui rendering error

--- modules/defaultui/localization/en/errors.ts
+++ modules/defaultui/localization/en/errors.ts
@@ -5,7 +5,7 @@ export const errors =
   genericErrorDescription: "An error has occurred in the game.",
   genericErrorCauseDescription: "This is probably due to a programming oversight in the game and shouldn't cause any damage that can't be fixed by reloading this page. The error may keep happening though.",
   checkConsolePrompt: "Open the developer console on your browser for more information.",
-  openConsoleInstructions: "Cmd/Ctrl+Shift+J or F12 on most desktop browsers.",
+  openConsoleInstructions: "Ctrl+Shift+J (Cmd+Shift+J) or F12 on most desktop browsers.",
   canTryToRecoverGame: "You can try to save the current game or load a previous save. (Depending on how bad the first error was, neither of these may work)",
-  errorWithGameRecovery: "An additional error occurred trying to render the UI for save game recovery. This was probably caused by the same thing as the first error.",
+  errorWithGameRecovery: "An additional error occurred trying to render the UI for save game recovery.",
 };
