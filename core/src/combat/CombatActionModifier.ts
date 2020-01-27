35c3674d4d58a61c4c944fe5ae512483d820459a
giraluna <giraluna3@gmail.com>
2020-01-27 08:11:28 +0200

    don't require combat action modifiers to have a key

--- core/src/combat/CombatActionModifier.ts
+++ core/src/combat/CombatActionModifier.ts
@@ -4,7 +4,6 @@ import { CombatActionPrimitivesWithValues } from "./CombatActionPrimitive";
 
 export interface CombatActionModifier
 {
-  key: string;
-  flags?: Set<string>;
   primitives: CombatActionPrimitivesWithValues<Partial<FlatAndMultiplierAdjustment>>;
+  flags?: Set<string>;
 }
