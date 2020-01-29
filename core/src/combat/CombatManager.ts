5fbc6260a13f1728599df945da084d80e29042e3
giraluna <giraluna3@gmail.com>
2020-01-29 11:00:25 +0200

    add queued actions to CombatManager

--- core/src/combat/CombatManager.ts
+++ core/src/combat/CombatManager.ts
@@ -1,11 +1,17 @@
 import { CombatPhase } from "./CombatPhase";
 import { CombatPhaseInfo } from "./CombatPhaseInfo";
+import { CombatAction } from "./CombatAction";
 
 
 
 export class CombatManager
 {
   public currentPhase: CombatPhase;
+
+  private readonly queuedActions:
+  {
+    [phaseKey: string]: CombatAction[];
+  } = {};
 
   constructor()
   {
@@ -15,5 +21,23 @@ export class CombatManager
   public setPhase(phaseInfo: CombatPhaseInfo): void
   {
     this.currentPhase = new CombatPhase(phaseInfo);
+    if (this.queuedActions[phaseInfo.key])
+    {
+      this.queuedActions[phaseInfo.key].forEach(queuedAction =>
+      {
+        this.currentPhase.addActionToBack(queuedAction);
+      });
+
+      this.queuedActions[phaseInfo.key] = [];
+    }
+  }
+  public addQueuedAction(action: CombatAction, phaseInfo: CombatPhaseInfo): void
+  {
+    if (!this.queuedActions[phaseInfo.key])
+    {
+      this.queuedActions[phaseInfo.key] = [];
+    }
+
+    this.queuedActions[phaseInfo.key].push(action);
   }
 }
