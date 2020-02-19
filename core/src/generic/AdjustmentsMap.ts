6c04be05741df261d5a128cf1613e06373d52cac
giraluna <giraluna3@gmail.com>
2020-02-19 11:47:00 +0200

    add AdjustmentsMap class
    used by new battle prep effects stuff

---
+++ core/src/generic/AdjustmentsMap.ts
@@ -0,0 +1,48 @@
+import { FlatAndMultiplierAdjustment, getBaseAdjustment, squashFlatAndMultiplierAdjustments, applyFlatAndMultiplierAdjustments } from "./FlatAndMultiplierAdjustment";
+import { MapBase } from "./MapBase";
+
+
+export class AdjustmentsMap<Key extends object> extends MapBase<
+  Key,
+  FlatAndMultiplierAdjustment,
+  Partial<FlatAndMultiplierAdjustment>
+>
+{
+  constructor()
+  {
+    super();
+  }
+
+  public map<NewKey extends object>(
+    mapFn: (key: Key) => NewKey,
+  ): AdjustmentsMap<NewKey>
+  {
+    return super.mapRaw(mapFn) as AdjustmentsMap<NewKey>;
+  }
+  public resolve(): Map<Key, number>
+  {
+    const resolved = new Map<Key, number>();
+
+    this.forEach((key, adjustment) =>
+    {
+      const resolvedAdjustment = applyFlatAndMultiplierAdjustments(0, adjustment);
+      resolved.set(key, resolvedAdjustment);
+    });
+
+    return resolved;
+  }
+
+  // tslint:disable-next-line: prefer-function-over-method
+  protected getDefaultValue(key: Key): FlatAndMultiplierAdjustment
+  {
+    return getBaseAdjustment();
+  }
+  // tslint:disable-next-line: prefer-function-over-method
+  protected squashValues(
+    existing: FlatAndMultiplierAdjustment,
+    toSquash: Partial<FlatAndMultiplierAdjustment>,
+  ): FlatAndMultiplierAdjustment
+  {
+    return squashFlatAndMultiplierAdjustments(existing, toSquash);
+  }
+}
---
+++ core/src/generic/MapBase.ts
@@ -0,0 +1,71 @@
+// TODO 2020.02.19 | probably a few classes that could be converted to use this
+export abstract class MapBase<
+  Key extends object,
+  Value extends AddableValue,
+  AddableValue,
+>
+{
+  private readonly _map: Map<Key, Value> = new Map();
+
+  constructor()
+  {
+
+  }
+
+  public get(key: Key): Value
+  {
+    if (this._map.has(key))
+    {
+      return this._map.get(key);
+    }
+    else
+    {
+      return this.getDefaultValue(key);
+    }
+  }
+  public add(key: Key, toAdd: AddableValue): void
+  {
+    const existing = this.get(key);
+    const squashed = this.squashValues(existing, toAdd);
+    this._map.set(key, squashed);
+  }
+  public forEach(callback: (key: Key, value: Value) => void): void
+  {
+    this._map.forEach((value, key) => callback(key, value));
+  }
+  public filter(filterFn: (key: Key, value: Value) => boolean): this
+  {
+    const filtered = this.constructor();
+
+    this.forEach((key, value) =>
+    {
+      if (filterFn(key, value))
+      {
+        filtered.add(key, value);
+      }
+    });
+
+    return <any>filtered;
+  }
+
+  // should be abstract and static, but not currently possible in typescript
+  // https://github.com/Microsoft/TypeScript/issues/14600
+  protected abstract getDefaultValue(key: Key): Value;
+  protected abstract squashValues(existing: Value, toSquash: AddableValue): Value;
+
+  // don't think it's possible to type this polymorphically without higher kinded types
+  // child class should define correct type signature and call this
+  protected mapRaw<NewKey extends object>(
+    mapFn: (key: Key) => NewKey,
+  ): MapBase<NewKey, Value, AddableValue>
+  {
+    const mapped: MapBase<NewKey, Value, AddableValue> = this.constructor();
+
+    this.forEach((key, value) =>
+    {
+      mapped.add(mapFn(key), value);
+    });
+
+    return mapped;
+  }
+}
