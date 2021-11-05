2afa220aaf7a3e38f2928d272d871f62eb17be4b
giraluna <giraluna3@gmail.com>
2021-11-05 21:33:24 +0200

    fix jest configs

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleDirectories: ["node_modules", "<rootDir>"],
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig-tests.json'
--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,4 @@ const base = require("./jest.config.base");
 
 module.exports = {
   ...base,
-  projects: [
-    "<rootDir>/core/test/jest.config.js",
-    "<rootDir>/modules/baselib/test/jest.config.js",
-  ],
 };
