3034d8ba31636c71b0fa491c681d06398546abd5
giraluna <giraluna3@gmail.com>
2021-11-16 12:57:56 +0200

    add tests for CombatActionResultModifier

--- core/test/combat/CombatActionResultModifier.test.ts
+++ core/test/combat/CombatActionResultModifier.test.ts
@@ -1,10 +1,58 @@
+import { CombatActionResultModifierWithValue, getOrderedResultModifiers } from "core/src/combat/CombatActionResultModifier";
+
 describe("CombatActionResultModifier", () =>
 {
+  // probably belong in OrderedGraph tests instead
   describe("getOrderedResult", () =>
   {
-    it("throws when order of modifiers forms a cycle", () =>
+    function makeDummyModifier(key: string, ...parents: string[]): CombatActionResultModifierWithValue<number>
     {
-      // TODO 2021.11.12 | implement
+      return {
+        modifier:
+        {
+          key: key,
+          flags: new Set([key]),
+          flagsThatShouldBeExecutedBefore: parents,
+          modifyResult: () => {},
+        },
+        value: 1,
+      };
+    }
+    it("throws when all modifiers form a cycle", () =>
+    {
+      expect(() =>
+      {
+        getOrderedResultModifiers(
+        [
+          makeDummyModifier("a", "b"),
+          makeDummyModifier("b", "c"),
+          makeDummyModifier("c", "a"),
+        ]);
+      }).toThrow("No starting node could be found");
+    });
+    it("throws when a group connected to a valid parent forms a cycle", () =>
+    {
+      expect(() =>
+      {
+        getOrderedResultModifiers(
+        [
+          makeDummyModifier("a"),
+          makeDummyModifier("b", "c", "a"),
+          makeDummyModifier("c", "b"),
+        ]);
+      }).toThrow("Cyclical ordering");
+    });
+    it("throws when an isolated group forms a cycle", () =>
+    {
+      expect(() =>
+      {
+        getOrderedResultModifiers(
+        [
+          makeDummyModifier("a"),
+          makeDummyModifier("b", "c"),
+          makeDummyModifier("c", "b"),
+        ]);
+      }).toThrow("isolated nodes that form a cycle");
     });
   });
 });
